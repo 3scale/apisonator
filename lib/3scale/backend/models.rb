0e6ff4bd11d21bb0bc6c0a80618b3fb2aef67b85
Adam Cigánek <adam.ciganek@gmail.com>
2010-05-07 12:58:12 +0200

    Removes all SQL and ActiveRecord related stuff

--- lib/3scale/backend/models.rb
+++
@@ -1,9 +0,0 @@
-require 'active_record'
-
-ActiveRecord::Base.establish_connection(ThreeScale::Backend.configuration.sql)
-
-require '3scale/backend/models/account'
-require '3scale/backend/models/contract'
-require '3scale/backend/models/metric'
-require '3scale/backend/models/plan'
-require '3scale/backend/models/service'
