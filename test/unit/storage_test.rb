a4b9fb2a01b347b7448b00e8162b5066c910ac01
solso <josep@3scale.net>
2012-11-13 16:47:34 +0100

    remove no longer needed tests for failover

--- test/unit/storage_test.rb
+++ test/unit/storage_test.rb
@@ -12,30 +12,4 @@ class StorageTest < Test::Unit::TestCase
     assert_equal 'bar', @storage.get('foo')
   end
 
-  def test_restore_backup_replays_the_commands_from_the_backup_file
-    write_backup_file('set foo stuff', 'set bar junk')
-
-    @storage.restore_backup
-    assert_equal 'stuff', @storage.get('foo')
-    assert_equal 'junk',  @storage.get('bar')
-  end
-
-  def test_restore_backup_deletes_the_backup_file
-    write_backup_file('set foo stuff')
-    
-    @storage.restore_backup
-    assert !File.exists?(Storage::Failover::DEFAULT_BACKUP_FILE), 'File should not exist'
-  end
-
-  private
-
-  def write_backup_file(*commands)
-    FileUtils.mkdir_p(File.dirname(Storage::Failover::DEFAULT_BACKUP_FILE))
-
-    File.open(Storage::Failover::DEFAULT_BACKUP_FILE, 'w') do |io|
-      commands.each do |command|
-        io << command << "\n"
-      end
-    end
-  end
 end
