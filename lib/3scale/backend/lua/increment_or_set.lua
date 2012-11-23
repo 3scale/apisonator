c5ac7808fa8952f847e3e3651875918b3db63aad
Jakub Hozak <jakub.hozak@gmail.com>
2012-11-23 12:18:12 +0100

    Revert "test fixed"
    This reverts commit 0327f25ff921291ee093df3a88b0bc7704c9906e.

--- lib/3scale/backend/lua/increment_or_set.lua
+++ lib/3scale/backend/lua/increment_or_set.lua
@@ -12,6 +12,7 @@ local timestamp_day = ARGV[11]
 local timestamp_hour = ARGV[12]
 local timestamp_minute = ARGV[13]
 
+
 local service_prefix     = "stats/{service:" .. service_id .. "}"
 local application_prefix = service_prefix .. "/cinstance:" .. application_id
 
@@ -26,41 +27,37 @@ local service_metric_prefix = service_prefix .. "/metric:" .. metric_id
 local application_metric_prefix = application_prefix .. "/metric:" .. metric_id
 local prefixes = { application_metric_prefix }
 
-
-
-if not tostring(user_id):find("^%s*$") then
+if user_id ~= nil then
    local user_prefix     =  service_prefix .. "/uinstance:" .. user_id
    local user_metric_prefix = user_prefix .. "/metric:" .. metric_id
-   table.insert(prefixes,  user_metric_prefix)
+   prefixes['user_metric_prefix'] =  user_metric_prefix
 end
 
 local granularities = { eternity=timestamp_et, year=timestamp_year,
 			month=timestamp_month, week=timestamp_week, day=timestamp_day,
 			hour=timestamp_hour , minute=timestamp_minute }
 
+-- -- eternity
+-- redis.call(action, key, value)
+
+-- granularities
 for granularity,timestamp in pairs(granularities) do
    for i,prefix in ipairs(prefixes) do
-
-      local key = prefix .. timestamp
-       redis.call(action, key, value)
-
+      local key = prefix ..  timestamp
+      redis.call(action,key,value)
       if granularity == 'minute'  then
 	 redis.call('expire', key, 180)
       end
    end
-
 end
 
 
 granularities["year"] = nil
 granularities["minute"] = nil
 for granularity,timestamp in pairs(granularities) do
-
    local prefix = service_metric_prefix
    local key = prefix .. timestamp
    redis.call(action,key,value)
 end
-
-
 
 -- redis.call(action,key,value)
