461b20d79f9e3ea484ad3bf9955686dac3ee9099
solso <josep@3scale.net>
2013-01-03 15:43:37 +0100

    More complete benchmark, workers and listeners
    WITH MEMOIZING
    
    ```
    solso@atari:~/3scale/backend(memoizing) $ bundle exec ruby benchmarks/test_all_perf.rb
    Backend version: 1.6.31
    Resque version: 1.23.0
    Redis version: 3.0.2
    Filling seed...
    done.
    Starting test...
          user     system      total        real
    adding transactions:   1.240000   0.080000   1.320000 (  1.462248)
    processing priority:   4.550000   0.380000   4.930000 (  5.982068)
    processing main:       0.850000   0.050000   0.900000 (  1.036302)
    do authrep with caching:   1.650000   0.060000   1.710000 (  1.843126)
    do authorize with caching:   1.030000   0.070000   1.100000 (  1.256600)
    do authrep without caching:   2.540000   0.120000   2.660000 (  2.930592)
    do authorize without caching:   1.920000   0.120000   2.040000 (  2.330887)
    
    Redis commands:
    adding transactions:  4.006 redis commands per request
    processing priority:  88.147 redis commands per request
    processing main:  3.013 redis commands per request
    do authrep with caching:  6.011 redis commands per request
    do authorize with caching:  4.008 redis commands per request
    do authrep without caching:  10.002 redis commands per request
    do authorize without caching:  8.001 redis commands per request
    ```
    
    WITHOUT MEMOIZING
    
    ```
    solso@atari:~/3scale/backend(memoizing) $ bundle exec ruby benchmarks/test_all_perf.rb
    Backend version: 1.6.31
    Resque version: 1.23.0
    Redis version: 3.0.2
    Filling seed...
    done.
    Starting test...
          user     system      total        real
    adding transactions:   1.410000   0.130000   1.540000 (  1.795222)
    processing priority:   6.740000   0.750000   7.490000 (  9.461599)
    processing main:       0.890000   0.080000   0.970000 (  1.112376)
    do authrep with caching:   1.670000   0.070000   1.740000 (  1.921527)
    do authorize with caching:   1.060000   0.090000   1.150000 (  1.335609)
    do authrep without caching:   4.720000   0.500000   5.220000 (  6.412034)
    do authorize without caching:   4.150000   0.510000   4.660000 (  5.872251)
    
    Redis commands:
    adding transactions:  7.003 redis commands per request
    processing priority:  112.128 redis commands per request
    processing main:  4.012 redis commands per request
    do authrep with caching:  7.031 redis commands per request
    do authorize with caching:  5.032 redis commands per request
    do authrep without caching:  35.002 redis commands per request
    do authorize without caching:  33.001 redis commands per request
    ```

---
+++ benchmarks/test_all_perf.rb
@@ -0,0 +1,171 @@
+#puts "Using gemset: "
+#system "rvm current"
+
+require '3scale/backend'
+require 'benchmark'
+require 'fileutils'
+require 'nokogiri'
+require 'ruby-debug'
+
+puts "Backend version: #{ThreeScale::Backend::VERSION}"
+puts "Resque version: #{Resque::VERSION}"
+puts "Redis version: #{Redis::VERSION}"
+
+redis = ThreeScale::Backend::Storage.instance
+redis.flushdb
+
+puts "Filling seed..."
+system "rake seed_user"
+puts "done."
+
+## DATA
+@provider_key = "pkey"
+@app_id = "app_id"
+@service_id = "1001"
+@user_id = "foo"
+@usage = {"hits" => 4, "other" => 1, "method2" => 1}
+
+def assert_equal(a, b)
+  if a!=b
+    raise "Assert failed: #{a} != #{b}" if a!=b
+  end
+end
+
+def add_transaction
+  transactions = {0 => {:app_id => @app_id, :usage => @usage.symbolize_keys, :user_id => @user_id}}
+  ThreeScale::Backend::Transactor.report(@provider_key,@service_id,transactions)  
+end
+
+def do_authrep
+  params = {}
+  params[:usage] = @usage
+  params[:user_id] = @user_id
+  params[:app_id] = @app_id    
+  ThreeScale::Backend::Transactor.authrep(@provider_key, params)
+end
+
+def do_authorize
+  params = {}
+  params[:usage] = @usage
+  params[:user_id] = @user_id
+  params[:app_id] = @app_id  
+  ThreeScale::Backend::Transactor.authorize(@provider_key, params)
+end
+
+
+FileUtils.remove_file("/tmp/3scale_backend_workers_from_test_workers_perf.log", :force => true)
+
+raise "Assert failed" unless redis.llen("resque:queue:main")==0
+raise "Assert failed" unless redis.llen("resque:queue:priority")==0
+
+N = 100
+
+puts "Starting test..."
+
+redis_commands = Array.new
+labels = [""]
+
+Benchmark.bm do |x|
+  
+  labels << "adding transactions: "
+  redis_commands << ThreeScale::Backend::Storage.instance.info["total_commands_processed"].to_i  
+  x.report("adding transactions: ") { N.times {add_transaction }}
+    
+  assert_equal redis.llen("resque:queue:main"), N
+  assert_equal redis.llen("resque:queue:priority"), N
+  
+  @worker = ThreeScale::Backend::Worker.new(:one_off => true, :log_file => "/tmp/3scale_backend_workers_from_test_workers_perf.log")
+  
+  labels << "processing priority: " 
+  redis_commands << ThreeScale::Backend::Storage.instance.info["total_commands_processed"].to_i
+  x.report("processing priority: ") { (N).times { @worker.work }}
+
+  assert_equal redis.llen("resque:queue:main"), N
+  assert_equal redis.llen("resque:queue:priority"), 0
+  
+  assert_equal redis.get("stats/{service:1001}/cinstance:app_id/metric:8001/eternity"), (N*5).to_s
+  assert_equal redis.get("stats/{service:1001}/metric:8001/eternity"), (N*5).to_s
+  
+  assert_equal redis.get("stats/{service:1001}/cinstance:app_id/metric:8002/eternity"), N.to_s
+  assert_equal redis.get("stats/{service:1001}/metric:8002/eternity"), N.to_s
+  
+  assert_equal redis.get("stats/{service:1001}/cinstance:app_id/metric:80012/eternity"), N.to_s
+  assert_equal redis.get("stats/{service:1001}/metric:80012/eternity"), N.to_s
+
+  assert_equal redis.get("stats/{service:1001}/uinstance:foo/metric:8001/eternity"), (N*5).to_s
+  assert_equal redis.get("stats/{service:1001}/uinstance:foo/metric:8002/eternity"), N.to_s  
+  assert_equal redis.get("stats/{service:1001}/uinstance:foo/metric:80012/eternity"), N.to_s
+
+  labels << "processing main: "
+  redis_commands << ThreeScale::Backend::Storage.instance.info["total_commands_processed"].to_i
+  x.report("processing main:     ") { (N).times { @worker.work  }}
+
+  assert_equal redis.llen("resque:queue:main"), 0
+  assert_equal redis.llen("resque:queue:priority"), 0
+  
+  assert_equal redis.get("stats/{service:1001}/cinstance:app_id/metric:8001/eternity"), (N*5).to_s
+  assert_equal redis.get("stats/{service:1001}/metric:8001/eternity"), (N*5).to_s
+  
+  assert_equal redis.get("stats/{service:1001}/cinstance:app_id/metric:8002/eternity"), N.to_s
+  assert_equal redis.get("stats/{service:1001}/metric:8002/eternity"), N.to_s
+  
+  assert_equal redis.get("stats/{service:1001}/cinstance:app_id/metric:80012/eternity"), N.to_s
+  assert_equal redis.get("stats/{service:1001}/metric:80012/eternity"), N.to_s
+  
+  assert_equal redis.get("stats/{service:1001}/uinstance:foo/metric:8001/eternity"), (N*5).to_s
+  assert_equal redis.get("stats/{service:1001}/uinstance:foo/metric:8002/eternity"), N.to_s  
+  assert_equal redis.get("stats/{service:1001}/uinstance:foo/metric:80012/eternity"), N.to_s
+  
+  labels << "do authrep with caching: "
+  redis_commands << ThreeScale::Backend::Storage.instance.info["total_commands_processed"].to_i
+  x.report("do authrep with caching: ") { (N-1).times {do_authrep }}
+  
+  response = do_authrep  
+  doc = Nokogiri::XML(response[1])
+  assert_equal 'true', doc.at('status:root authorized').content
+  
+  labels << "do authorize with caching: "
+  redis_commands << ThreeScale::Backend::Storage.instance.info["total_commands_processed"].to_i
+  x.report("do authorize with caching: ") { (N-1).times {do_authorize }}
+  
+  response = do_authorize
+  doc = Nokogiri::XML(response[1])
+  assert_equal 'true', doc.at('status:root authorized').content
+    
+  ThreeScale::Backend::Transactor.caching_disable
+  
+  labels << "do authrep without caching: "
+  redis_commands << ThreeScale::Backend::Storage.instance.info["total_commands_processed"].to_i
+  x.report("do authrep without caching: ") { (N-1).times {do_authrep }}
+  
+  response = do_authrep
+  doc = Nokogiri::XML(response.first.to_xml)
+  assert_equal 'true', doc.at('status:root authorized').content
+  
+  ThreeScale::Backend::Transactor.caching_disable
+  
+  labels << "do authorize without caching: "
+  redis_commands << ThreeScale::Backend::Storage.instance.info["total_commands_processed"].to_i
+  x.report("do authorize without caching: ") { (N-1).times {do_authorize }}
+  
+  response = do_authorize
+  doc = Nokogiri::XML(response.first.to_xml)
+  assert_equal 'true', doc.at('status:root authorized').content
+  
+  system "wc -l /tmp/3scale_backend_workers_from_test_workers_perf.log > /tmp/temp_wc_l.tmp"
+  num_entries_log = File.new("/tmp/temp_wc_l.tmp","r").read.to_i
+  assert_equal (N*2)+1, num_entries_log
+  
+  redis_commands << ThreeScale::Backend::Storage.instance.info["total_commands_processed"].to_i
+  puts "\nRedis commands:"
+  i=1
+  while i < redis_commands.size do
+    puts "#{labels[i]} #{(redis_commands[i] - redis_commands[i-1]) / N.to_f} redis commands per request"
+    i=i+1
+  end
+  
+  ThreeScale::Backend::Transactor.caching_enable
+  
+  
+end  
+
