c512c8029aed275edc1cf20ca9e5f63005f99cfd
solso <josep@3scale.net>
2013-03-18 13:01:54 +0100

    Fixes for invalid encoding and large parameters

--- lib/3scale/backend/rack_exception_catcher.rb
+++ lib/3scale/backend/rack_exception_catcher.rb
@@ -15,9 +15,21 @@ module Rack
           [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::BadRequest.new().to_xml ]]
         elsif e.class == ArgumentError && e.message == "invalid byte sequence in UTF-8"
           [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]]
-        #elsif e.class == ArgumentError && e.message == "invalid %-encoding"
-        #  [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]] 
+        elsif e.class == ArgumentError && e.message =~ /invalid .-encoding/
+          [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]] 
+        elsif e.class == RangeError && e.message == "exceeded available parameter key space"
+          [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]] 
         else
+          ## temporal monitor
+          begin
+            f = ::File.new("/mnt/tmp_rack_weird_exceptions.log","a")
+            f.puts "------"
+            f.puts Time.now.utc.to_s
+            f.puts env.inspect
+            f.puts e
+            f.close
+          rescue Exception => exc
+          end  
           raise e
         end
       end
--- lib/3scale/backend/version.rb
+++ lib/3scale/backend/version.rb
@@ -1,5 +1,5 @@
 module ThreeScale
   module Backend
-    VERSION = '1.7.8'
+    VERSION = '1.7.9'
   end
 end
