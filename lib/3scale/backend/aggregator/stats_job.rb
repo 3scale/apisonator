0ad4d7938ddb78fa64dcbbf6dab95672dc54d49e
Toni Reina <areina0@gmail.com>
2013-10-09 11:57:22 +0200

    Add profiling logs.

--- lib/3scale/backend/aggregator/stats_job.rb
+++ lib/3scale/backend/aggregator/stats_job.rb
@@ -18,8 +18,14 @@ module ThreeScale
             ## it will save all the changed keys from the oldest time bucket. If it
             ## fails it will put the bucket on the stats:failed so that it can be processed
             ## one by one via rake task
+
             Aggregator.save_to_mongo(b)
           end
+
+          object_counts = Hash.new(0)
+          ObjectSpace.each_object(Object) { |object| object_counts[object.class] += 1}
+          Worker.logger.info("PROF: #{object_counts.sort_by { |a, b2| -b2 }[1..15].inspect}")
+          Worker.logger.info("PROF: Number of symbols #{Symbol.all_symbols.size}")
 
           end_time = Time.now.getutc
           Worker.logger.info("StatsJob #{bucket} #{buckets_to_save.size} #{(end_time-start_time).round(5)} #{(end_time.to_f-enqueue_time).round(5)}")
