f88c05b454f5b0caed21bcb9a6066a6020cff190
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-03-13 12:37:01 +0100

    Filter input params.

--- app/api/internal/services_api.rb
+++ app/api/internal/services_api.rb
@@ -2,25 +2,33 @@ module ThreeScale
   module Backend
     class ServicesAPI < InternalAPI
       before do
+        filter_params
         content_type 'application/json'
       end
 
       get '/' do
-        service = if params['provider_key']
+        get_service(params).to_json
+      end
+
+      get '/list_ids' do
+        Service.list(params['provider_key']).to_json
+      end
+
+      private
+
+      def get_service(options)
+        if params['provider_key']
           Service.load params['provider_key']
         elsif params['id']
           Service.load_by_id params['id']
         else
           nil
         end
-        service.to_json
       end
 
-      get '/list_ids' do
-        list = Service.list(params['provider_key'])
-        list.to_json
+      def filter_params
+        params.reject!{ |k, v| !['provider_key', 'id'].include? k }
       end
-
     end
   end
 end
