3b56ec7bc6e8603abea72b16bb166835377f8382
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-03-26 12:40:20 +0100

    Symbolize keys.

--- app/api/internal/services_api.rb
+++ app/api/internal/services_api.rb
@@ -7,6 +7,7 @@ module ThreeScale
         content_type 'application/json'
         parse_json_params params
         filter_params params
+        massage_params params
       end
 
       get '/:id' do
@@ -53,6 +54,21 @@ module ThreeScale
 
       def filter_params(params)
         params.reject!{ |k, v| !ACCEPTED_PARAMS.include? k }
+      end
+
+      # Symbolizes keys.
+      def massage_params(params)
+        params.keys.each do |key|
+          unless key.is_a? Symbol
+            params[key.to_sym] = params[key]
+            params.delete key
+            key = key.to_sym
+          end
+
+          if params[key].is_a? Hash
+            massage_params params[key]
+          end
+        end
       end
     end
   end
