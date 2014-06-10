0edf2ff85786023f506fbe8265500d59f29fcb52
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-06-10 17:29:01 +0200

    Hide encoding errors from Airbrake

--- lib/3scale/backend/rack_exception_catcher.rb
+++ lib/3scale/backend/rack_exception_catcher.rb
@@ -6,7 +6,7 @@ module Rack
       @app = app
       @options = options
     end
-    
+
     def call(env)
       begin
         @app.call(env)
@@ -14,15 +14,27 @@ module Rack
         if e.class == TypeError
           [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::BadRequest.new().to_xml ]]
         elsif e.class == ArgumentError && e.message == "invalid byte sequence in UTF-8"
+          delete_sinatra_error! env
           [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]]
         elsif e.class == ArgumentError && e.message =~ /invalid .-encoding/
-          [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]] 
+          delete_sinatra_error! env
+          [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]]
         elsif e.class == RangeError && e.message == "exceeded available parameter key space"
-          [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]] 
+          [400, { 'Content-Type' => 'application/vnd.3scale-v2.0+xml' }, [ ThreeScale::Backend::NotValidData.new().to_xml ]]
         else
           raise e
         end
       end
+    end
+
+    private
+
+    # Deletes 'sinatra.error' key in Rack's env hash.
+    # Newer version of airbrake gem is reporting 'sinatra.error' and we don't
+    # want it when the error is rescued and managed by us with the error handler.
+    # @return [nil]
+    def delete_sinatra_error!(env)
+      env['sinatra.error'] = nil
     end
   end
 
