ef9ab2970dc61a3a54f8dace5991a81ee2c794ed
Alejandro Martinez Ruiz <alex@flawedcode.org>
2014-08-21 10:55:03 +0200

    internal_api: drop massage_params since JSON.parse can already do that for us

--- app/api/internal/internal_api.rb
+++ app/api/internal/internal_api.rb
@@ -5,7 +5,6 @@ module ThreeScale
       before do
         content_type 'application/json'
         parse_json_params params
-        massage_params params
       end
 
       get '/check.json' do
@@ -16,26 +15,11 @@ module ThreeScale
 
       def parse_json_params(params)
         body = request.body.read
-        params.merge! JSON.parse(body) unless body.empty?
+        params.merge! JSON.parse(body, symbolize_names: true) unless body.empty?
       end
 
       def filter_params(params)
         params.reject!{ |k, v| !ACCEPTED_PARAMS.include? k }
-      end
-
-      # Symbolizes keys.
-      def massage_params(params)
-        params.keys.each do |key|
-          unless key.is_a? Symbol
-            params[key.to_sym] = params[key]
-            params.delete key
-            key = key.to_sym
-          end
-
-          if params[key].is_a? Hash
-            massage_params params[key]
-          end
-        end
       end
 
       def respond_with_400(exception)
