98a5e23d9154022aeabaef873cb3efbd1b413f45
Alejandro Martinez Ruiz <alex@flawedcode.org>
2014-08-27 17:43:26 +0200

    test/memoizer: completed testing full functionality of Memoizer

--- test/unit/memoizer_test.rb
+++ test/unit/memoizer_test.rb
@@ -5,22 +5,153 @@ class MemoizerTest < Test::Unit::TestCase
     Memoizer.reset!
   end
 
-  def test_memoizer_block_storage
-    key = 'simple key'
-    assert_nil Memoizer.get(key)
-
-    Memoizer.memoize_block(key) { :foo }
-
-    assert_equal :foo, Memoizer.get(key)
+  def key
+    'simple_key'
   end
 
-  def test_memoizer_storage_clear
-    Memoizer.memoize :foo, :bar
-    assert_equal :bar, Memoizer.get(:foo)
+  def value
+    'some_value'
+  end
 
-    Memoizer.clear(:foo)
-    assert !Memoizer.memoized?(:foo)
-    assert_nil Memoizer.get(:foo)
+  def another_key
+    'another_key'
+  end
+
+  def another_value
+    'another_value'
+  end
+
+  def yet_another_key
+    'yet_another_key'
+  end
+
+  def yet_another_value
+    'yet_another_value'
+  end
+
+  def keys_values
+    {
+      key => value,
+      another_key => another_value,
+      yet_another_key => yet_another_value
+    }
+  end
+
+  def test_memoizer_get
+    Memoizer.clear key
+    assert_nil Memoizer.get(key)
+    Memoizer.memoize key, value
+    assert_equal value, Memoizer.get(key)
+    Memoizer.memoize key, nil
+    assert_nil Memoizer.get(key)
+  end
+
+  def test_memoizer_block
+    Memoizer.clear(key)
+    assert_nil Memoizer.get(key)
+    Memoizer.memoize_block(key) { value }
+    assert_equal value, Memoizer.get(key)
+  end
+
+  def test_memoizer_clear
+    keys_values.each do |k, v|
+      Memoizer.memoize k, v
+    end
+    Memoizer.clear [key, another_key]
+    assert !Memoizer.memoized?(key)
+    assert_nil Memoizer.get(key)
+    assert !Memoizer.memoized?(another_key)
+    assert_nil Memoizer.get(another_key)
+    assert Memoizer.memoized?(yet_another_key)
+    assert_equal yet_another_value, Memoizer.get(yet_another_key)
+  end
+
+  def test_memoizer_memoize
+    Memoizer.clear key
+    Memoizer.memoize key, value
+    assert_equal value, Memoizer.get(key)
+  end
+
+  def test_memoizer_memoized?
+    Memoizer.clear [key, another_key]
+    Memoizer.memoize key, value
+    assert Memoizer.memoized?(key)
+    assert !Memoizer.memoized?(another_key)
+    expiretime = Memoizer.const_get :EXPIRE
+    Memoizer.memoize another_key, another_value
+    assert Memoizer.memoized?(another_key)
+    Timecop.travel(Time.now + expiretime + 1) do
+      assert !Memoizer.memoized?(another_key)
+    end
+  end
+
+  def test_memoizer_reset!
+    Memoizer.memoize key, value
+    Memoizer.reset!
+    assert !Memoizer.memoized?(key)
+    Memoizer.memoize key, value
+    Memoizer.memoize another_key, another_value
+    5.times { Memoizer.get key }
+    3.times { Memoizer.get another_key }
+    7.times { Memoizer.get yet_another_key }
+    Memoizer.reset!
+    assert_equal 0, Memoizer.stats[:hits]
+    assert_equal 0, Memoizer.stats[:count]
+    assert_equal 0, Memoizer.stats[:size]
+  end
+
+  def test_memoizer_stats
+    gets_per_key = { key => 3, another_key => 7, yet_another_key => 5 }
+    memoized_keys = Hash[keys_values.to_a.sample(gets_per_key.size - 1)]
+    Memoizer.reset!
+    memoized_keys.each do |k, v|
+      Memoizer.memoize k, v
+    end
+    gets_per_key.each do |k, g|
+      g.times do
+        Memoizer.memoized? k
+      end
+    end
+    hits = memoized_keys.reduce(0) do |acc, (k, _)|
+      acc + gets_per_key[k]
+    end
+    assert_equal hits, Memoizer.stats[:hits]
+    count = gets_per_key.reduce(0) { |acc, (_, v)| acc + v }
+    assert_equal count, Memoizer.stats[:count]
+    assert_equal memoized_keys.size, Memoizer.stats[:size]
+  end
+
+  def test_memoizer_purge
+    expiretime = Memoizer.const_get :EXPIRE
+    Memoizer.reset!
+    Memoizer.memoize key, value
+    Memoizer.purge Time.now.utc.to_i + expiretime + 1
+    assert !Memoizer.memoized?(key)
+  end
+
+  def test_memoizer_purge_max_entries
+    max_entries = Memoizer.const_get :MAX_ENTRIES
+    (max_entries+1).times do |i|
+      Memoizer.memoize i.to_s, value
+    end
+    Memoizer.purge(Time.now.utc.to_i)
+    assert_equal 0, Memoizer.stats[:size]
+  end
+
+  def test_memoizer_build_key
+    # it is probably debatable whether a key should have a specific type
+    assert Memoizer.build_key(self, :some_method, :some, :args).kind_of?(String)
+  end
+
+  def test_memoized_build_keys_for_class
+    keys = Memoizer.build_keys_for_class(self,
+                                         some_method: [:some, :args],
+                                         another_method: [:arg],
+                                         yet_another: [])
+    assert_equal 3, keys.size
+    keys.each do |k|
+      assert k.kind_of?(String)
+    end
   end
 
   def test_memoizer_decorator_instance_method
