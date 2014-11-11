4e95017876351f1a79ff4afc74941bd8e9bd57aa
Alejandro Martinez Ruiz <alex@flawedcode.org>
2014-11-11 18:14:19 +0100

    user: stop inheriting from Core
    While at it also refactor slightly to mimic other classes and solve
    a problem with adding a user to a service which was being done
    differently in BE and Core.

--- lib/3scale/backend/user.rb
+++ lib/3scale/backend/user.rb
@@ -1,6 +1,135 @@
 module ThreeScale
   module Backend
-    class User < Core::User
+    module CoreUser
+      def self.included(base)
+        base.include InstanceMethods
+        base.extend ClassMethods
+      end
+
+      module InstanceMethods
+        def save
+          service = Service.load_by_id(service_id)
+
+          save_attributes
+
+          ServiceUserManagementUseCase.new(service, username).add
+        end
+
+        def active?
+          state == :active
+        end
+
+        def key
+          self.class.key(service_id, username)
+        end
+
+        def storage_key
+          self.class.storage_key(service_id, username, attribute)
+        end
+
+        private
+
+        def save_attributes
+          storage.hset key, "state", state.to_s if state
+          storage.hset key, "plan_id", plan_id if plan_id
+          storage.hset key, "plan_name", plan_name if plan_name
+          storage.hset key, "username", username if username
+          storage.hset key, "service_id", service_id if service_id
+          storage.hincrby key, "version", 1
+        end
+      end
+
+      module ClassMethods
+        def load(service_id, username)
+          key = self.key(service_id, username)
+
+          values = storage.hmget(key, 'state', 'plan_id', 'plan_name', 'version')
+          state, plan_id, plan_name, vv = values
+
+          user = nil
+          unless state.nil?
+            user = new(service_id: service_id,
+                       username: username,
+                       state: state.to_sym,
+                       plan_id: plan_id,
+                       plan_name: plan_name)
+
+            incr_version(service_id, username) if vv.nil?
+          end
+
+          return user
+        end
+
+        def load_or_create!(service, username)
+          user = load(service.id, username)
+
+          if user.nil?
+            # the user does not exist yet, we need to create it for the case of
+            # the open loop
+            if service.user_registration_required?
+              raise ServiceRequiresRegisteredUser, service.id
+            else
+              raise ServiceRequiresDefaultUserPlan, service.id if service.default_user_plan_id.nil? ||
+                service.default_user_plan_name.nil?
+              state = 'active' if state.nil?
+            end
+
+            user = new(service_id: service.id,
+                       username: username,
+                       state: state.to_sym,
+                       plan_id: service.default_user_plan_id,
+                       plan_name: service.default_user_plan_name)
+            user.save
+          end
+
+          return user
+        end
+
+        def save!(attributes)
+          raise UserRequiresUsername if attributes[:username].nil?
+          raise UserRequiresServiceId if attributes[:service_id].nil?
+          service = Service.load_by_id(attributes[:service_id])
+          raise UserRequiresValidService if service.nil?
+          attributes[:plan_id] ||= service.default_user_plan_id
+          attributes[:plan_name] ||= service.default_user_plan_name
+          raise UserRequiresDefinedPlan if attributes[:plan_id].nil? || attributes[:plan_name].nil?
+          attributes[:state] = "active" if attributes[:state].nil?
+          user = new(attributes)
+          user.save
+          return user
+        end
+
+        def get_version(service_id, username)
+          storage.hget(self.key(service_id, username), 'version')
+        end
+
+        def incr_version(service_id, username)
+          storage.hincrby(self.key(service_id, username), 'version', 1)
+        end
+
+        def delete!(service_id, username)
+          service = Service.load_by_id(service_id)
+          raise UserRequiresValidService if service.nil?
+          ServiceUserManagementUseCase.new(service, username).delete
+          storage.del(self.key(service_id, username))
+        end
+
+        def key(service_id, username)
+          "service:#{service_id}/user:#{username}"
+        end
+
+        def storage_key(service_id, username, attribute)
+          "service:#{service_id}/user:#{username}/#{attribute}"
+        end
+      end
+    end
+
+    class User
+      include CoreUser
+      include Core::Storable
+
+      attr_accessor :service_id, :username, :state, :plan_id, :plan_name
+      attr_writer :version
 
       def self.load_or_create!(service, user_id)
         key = Memoizer.build_key(self, :load_or_create!, service.id, user_id)
@@ -23,20 +152,6 @@ module ThreeScale
 
       def usage_limits
         @usage_limits ||= UsageLimit.load_all(service_id, plan_id)
-      end
-
-      # Copy-pasted from Core
-      def save
-        service = Service.load_by_id(service_id)
-        ServiceUserManagementUseCase.new(service, username).add
-
-        storage.hset key, "state", state.to_s if state
-        storage.hset key, "plan_id", plan_id if plan_id
-        storage.hset key, "plan_name", plan_name if plan_name
-        storage.hset key, "username", username if username
-        storage.hset key, "service_id", service_id if service_id
-        storage.hincrby key, "version", 1
-
       end
 
       private
