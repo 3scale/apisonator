9b14889e6fa7fbbb8b7a90790945600cc2acc281
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-11-14 13:36:20 +0100

    Whitespace  cleanup

--- test/unit/log_request_storage_test.rb
+++ test/unit/log_request_storage_test.rb
@@ -6,7 +6,7 @@ class LogRequestStorageTest < Test::Unit::TestCase
   def setup
     @storage = Storage.instance(true)
     @storage.flushdb
-    
+
     @service_id     = next_id
     @application_id = next_id
     @metric_id      = next_id
@@ -15,13 +15,10 @@ class LogRequestStorageTest < Test::Unit::TestCase
   test '#store_all stores transactions' do
     service_id_one = @service_id
     service_id_two = next_id
-
     application_id_one = @application_id
     application_id_two = next_id
-
     metric_id_one = @metric_id
     metric_id_two = next_id
-
 
     log1 = {:service_id     => service_id_one,
             :application_id => application_id_one,
@@ -34,21 +31,14 @@ class LogRequestStorageTest < Test::Unit::TestCase
             :usage          => {metric_id_two => 2},
             :timestamp      => Time.utc(2010, 9, 10, 17, 10),
             :log            => {'request' => 'req2', 'response' => 'resp2', 'code' => 404}}
-
-
     LogRequestStorage.store_all([log1, log2])
 
-
     list = LogRequestStorage.list_by_service(service_id_one)
-
     assert_equal 2, list.size
     assert_equal 2, LogRequestStorage.count_by_service(service_id_one)
-
     ## the order is reversed, newer first
     assert_equal log1, list[1]
     assert_equal log2, list[0]
-
-
 
     list = LogRequestStorage.list_by_service(service_id_two)
     assert_equal 0, list.size
@@ -58,7 +48,6 @@ class LogRequestStorageTest < Test::Unit::TestCase
     assert_equal 1, list.size
     assert_equal 1, LogRequestStorage.count_by_application(service_id_one,application_id_one)
     assert_equal log1, list[0]
-
 
     list = LogRequestStorage.list_by_application(service_id_one,application_id_two)
     assert_equal 1, list.size
@@ -79,7 +68,6 @@ class LogRequestStorageTest < Test::Unit::TestCase
     LogRequestStorage.delete_by_service(service_id_one)
     list = LogRequestStorage.list_by_service(service_id_one)
     assert_equal 0, list.size
-
   end
 
   test '#store_all stores transactions that are not complete' do
@@ -88,10 +76,6 @@ class LogRequestStorageTest < Test::Unit::TestCase
 
     application_id_one = @application_id
     application_id_two = next_id
-
-    metric_id_one = @metric_id
-    metric_id_two = next_id
-
 
     log1 = {:service_id     => service_id_one,
             :application_id => application_id_one,
@@ -104,21 +88,14 @@ class LogRequestStorageTest < Test::Unit::TestCase
             :usage          => nil,
             :timestamp      => Time.utc(2010, 9, 10, 17, 10),
             :log            => {'request' => 'req2', 'response' => 'resp2', 'code' => 404}}
-
-
     LogRequestStorage.store_all([log1, log2])
 
-
     list = LogRequestStorage.list_by_service(service_id_one)
-
     assert_equal 2, list.size
     assert_equal 2, LogRequestStorage.count_by_service(service_id_one)
-
     ## the order is reversed, newer first
     assert_equal log1, list[1]
     assert_equal log2, list[0]
-
-
 
     list = LogRequestStorage.list_by_service(service_id_two)
     assert_equal 0, list.size
@@ -128,7 +105,6 @@ class LogRequestStorageTest < Test::Unit::TestCase
     assert_equal 1, list.size
     assert_equal 1, LogRequestStorage.count_by_application(service_id_one,application_id_one)
     assert_equal log1, list[0]
-
 
     list = LogRequestStorage.list_by_application(service_id_one,application_id_two)
     assert_equal 1, list.size
@@ -149,20 +125,14 @@ class LogRequestStorageTest < Test::Unit::TestCase
     LogRequestStorage.delete_by_service(service_id_one)
     list = LogRequestStorage.list_by_service(service_id_one)
     assert_equal 0, list.size
-
   end
-  
+
 
   test '#store_all stores transactions that are not complete on usage' do
     service_id_one = @service_id
     service_id_two = next_id
-
     application_id_one = @application_id
     application_id_two = next_id
-
-    metric_id_one = @metric_id
-    metric_id_two = next_id
-
 
     log1 = {:service_id     => service_id_one,
             :application_id => application_id_one,
@@ -176,10 +146,7 @@ class LogRequestStorageTest < Test::Unit::TestCase
             :timestamp      => Time.utc(2010, 9, 10, 17, 10),
             :log            => {'request' => 'req2'}}
 
-
     LogRequestStorage.store_all([log1, log2])
-
-
     list = LogRequestStorage.list_by_service(service_id_one)
 
     assert_equal 2, list.size
@@ -189,8 +156,6 @@ class LogRequestStorageTest < Test::Unit::TestCase
     assert_equal log1, list[1]
     assert_equal log2, list[0]
 
-
-
     list = LogRequestStorage.list_by_service(service_id_two)
     assert_equal 0, list.size
     assert_equal 0, LogRequestStorage.count_by_service(service_id_two)
@@ -199,7 +164,6 @@ class LogRequestStorageTest < Test::Unit::TestCase
     assert_equal 1, list.size
     assert_equal 1, LogRequestStorage.count_by_application(service_id_one,application_id_one)
     assert_equal log1, list[0]
-
 
     list = LogRequestStorage.list_by_application(service_id_one,application_id_two)
     assert_equal 1, list.size
@@ -220,26 +184,21 @@ class LogRequestStorageTest < Test::Unit::TestCase
     LogRequestStorage.delete_by_service(service_id_one)
     list = LogRequestStorage.list_by_service(service_id_one)
     assert_equal 0, list.size
-
   end
-  
-  
-  test 'respects the limits of the lists' do
 
+  test 'respects the limits of the lists' do
     log1 = {:service_id     => @service_id,
             :application_id => @application_id,
             :usage          => {"metric_id_one" => 1},
             :timestamp      => Time.utc(2010, 9, 10, 17, 4),
             :log            => {'request' => 'req', 'response' => 'resp', 'code' => 200}}
 
-    (LogRequestStorage::LIMIT_PER_SERVICE+LogRequestStorage::LIMIT_PER_APP).times do 
+    (LogRequestStorage::LIMIT_PER_SERVICE+LogRequestStorage::LIMIT_PER_APP).times do
       LogRequestStorage.store(log1)
     end
 
     assert_equal LogRequestStorage::LIMIT_PER_SERVICE, @storage.llen("logs/service_id:#{@service_id}")
     assert_equal LogRequestStorage::LIMIT_PER_APP, @storage.llen("logs/service_id:#{@service_id}/app_id:#{@application_id}")
-
-
   end
-
 end
+
