425264d5b5190d0a8fae9ce26c33cc109c0bf97b
Wojciech Ogrodowczyk <wojtek@7tonlnu.pl>
2014-11-14 17:46:38 +0100

    Remove redundant test for non-complete transactions
    The content of the :usage field doesn't have any result on storing
    (or not) of the transaction, so those tests are useless.

--- test/unit/log_request_storage_test.rb
+++ test/unit/log_request_storage_test.rb
@@ -12,64 +12,6 @@ class LogRequestStorageTest < Test::Unit::TestCase
     @metric_id      = next_id
   end
 
-  test '#store_all stores transactions that are not complete' do
-    service_id_one = @service_id
-    service_id_two = next_id
-
-    application_id_one = @application_id
-    application_id_two = next_id
-
-    log1 = {:service_id     => service_id_one,
-            :application_id => application_id_one,
-            :timestamp      => Time.utc(2010, 9, 10, 17, 4),
-            :usage          => "",
-            :log            => {'request' => 'req', 'response' => 'resp', 'code' => 200}}
-
-    log2 = {:service_id     => service_id_one,
-            :application_id => application_id_two,
-            :usage          => nil,
-            :timestamp      => Time.utc(2010, 9, 10, 17, 10),
-            :log            => {'request' => 'req2', 'response' => 'resp2', 'code' => 404}}
-    LogRequestStorage.store_all([log1, log2])
-
-    list = LogRequestStorage.list_by_service(service_id_one)
-    assert_equal 2, list.size
-    assert_equal 2, LogRequestStorage.count_by_service(service_id_one)
-    ## the order is reversed, newer first
-    assert_equal log1, list[1]
-    assert_equal log2, list[0]
-
-    list = LogRequestStorage.list_by_service(service_id_two)
-    assert_equal 0, list.size
-    assert_equal 0, LogRequestStorage.count_by_service(service_id_two)
-
-    list = LogRequestStorage.list_by_application(service_id_one,application_id_one)
-    assert_equal 1, list.size
-    assert_equal 1, LogRequestStorage.count_by_application(service_id_one,application_id_one)
-    assert_equal log1, list[0]
-
-    list = LogRequestStorage.list_by_application(service_id_one,application_id_two)
-    assert_equal 1, list.size
-    assert_equal 1, LogRequestStorage.count_by_application(service_id_one,application_id_two)
-    assert_equal log2, list[0]
-
-    LogRequestStorage.delete_by_application(service_id_one,application_id_two)
-    list = LogRequestStorage.list_by_application(service_id_one,application_id_two)
-    assert_equal 0, list.size
-
-    list = LogRequestStorage.list_by_service(service_id_one)
-    assert_equal 2, list.size
-
-    LogRequestStorage.delete_by_service(service_id_two)
-    list = LogRequestStorage.list_by_service(service_id_one)
-    assert_equal 2, list.size
-
-    LogRequestStorage.delete_by_service(service_id_one)
-    list = LogRequestStorage.list_by_service(service_id_one)
-    assert_equal 0, list.size
-  end
-
-
   test '#store_all stores transactions that are not complete on usage' do
     service_id_one = @service_id
     service_id_two = next_id
