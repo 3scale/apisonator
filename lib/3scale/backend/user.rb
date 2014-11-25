1a1301ece6dfdaef97fbd897650b2a1624921fe0
Alejandro Martinez Ruiz <alex@flawedcode.org>
2014-11-25 17:25:52 +0100

    user: integrate class methods module into the class

--- lib/3scale/backend/user.rb
+++ lib/3scale/backend/user.rb
@@ -1,27 +1,29 @@
 module ThreeScale
   module Backend
-    module CoreUser
-      def self.included(base)
-        base.extend ClassMethods
+    class User
+      include Core::Storable
+
+      attr_accessor :service_id, :username, :state, :plan_id, :plan_name
+      attr_writer :version
+
+      def self.load(service_id, username)
+        key = self.key(service_id, username)
+
+        values = storage.hmget(key, 'state', 'plan_id', 'plan_name', 'version')
+        state, plan_id, plan_name, vv = values
+
+        unless state.nil?
+          new(service_id: service_id, username: username,
+              state: state.to_sym, plan_id: plan_id,
+              plan_name: plan_name).tap do
+                incr_version(service_id, username) if vv.nil?
+              end
+        end
       end
 
-      module ClassMethods
-        def load(service_id, username)
-          key = self.key(service_id, username)
-
-          values = storage.hmget(key, 'state', 'plan_id', 'plan_name', 'version')
-          state, plan_id, plan_name, vv = values
-
-          unless state.nil?
-            new(service_id: service_id, username: username,
-                state: state.to_sym, plan_id: plan_id,
-                plan_name: plan_name).tap do
-              incr_version(service_id, username) if vv.nil?
-            end
-          end
-        end
-
-        def load_or_create!(service, username)
+      def self.load_or_create!(service, username)
+        key = Memoizer.build_key(self, :load_or_create!, service.id, username)
+        Memoizer.memoize_block(key) do
           user = load(service.id, username)
 
           unless user
@@ -45,67 +47,40 @@ module ThreeScale
 
           user
         end
-
-        def save!(attributes)
-          validate_attributes(attributes)
-
-          # create the user object
-          user = new(attributes)
-          user.save
-          user
-        end
-
-        def get_version(service_id, username)
-          storage.hget(self.key(service_id, username), 'version')
-        end
-
-        def incr_version(service_id, username)
-          storage.hincrby(self.key(service_id, username), 'version', 1)
-        end
-
-        def delete!(service_id, username)
-          service = Service.load_by_id(service_id)
-          raise UserRequiresValidService if service.nil?
-          ServiceUserManagementUseCase.new(service, username).delete
-          clear_cache(service_id, username)
-          storage.del(self.key(service_id, username))
-        end
-
-        def key(service_id, username)
-          "service:#{service_id}/user:#{username}"
-        end
-
-        def storage_key(service_id, username, attribute)
-          "service:#{service_id}/user:#{username}/#{attribute}"
-        end
-
-        private
-
-        def clear_cache(service_id, user_id)
-          key = Memoizer.build_key(self, :load_or_create!, service_id, user_id)
-          Memoizer.clear key
-        end
-
-        def validate_attributes(attributes)
-          raise UserRequiresUsername if attributes[:username].nil?
-          raise UserRequiresServiceId if attributes[:service_id].nil?
-          service = Service.load_by_id(attributes[:service_id])
-          raise UserRequiresValidService if service.nil?
-          attributes[:plan_id] ||= service.default_user_plan_id
-          attributes[:plan_name] ||= service.default_user_plan_name
-          raise UserRequiresDefinedPlan if attributes[:plan_id].nil? || attributes[:plan_name].nil?
-          attributes[:state] ||= "active"
-        end
-
       end
-    end
 
-    class User
-      include CoreUser
-      include Core::Storable
+      def self.save!(attributes)
+        validate_attributes(attributes)
 
-      attr_accessor :service_id, :username, :state, :plan_id, :plan_name
-      attr_writer :version
+        # create the user object
+        user = new(attributes)
+        user.save
+        user
+      end
+
+      def self.get_version(service_id, username)
+        storage.hget(self.key(service_id, username), 'version')
+      end
+
+      def self.incr_version(service_id, username)
+        storage.hincrby(self.key(service_id, username), 'version', 1)
+      end
+
+      def self.delete!(service_id, username)
+        service = Service.load_by_id(service_id)
+        raise UserRequiresValidService if service.nil?
+        ServiceUserManagementUseCase.new(service, username).delete
+        clear_cache(service_id, username)
+        storage.del(self.key(service_id, username))
+      end
+
+      def self.key(service_id, username)
+        "service:#{service_id}/user:#{username}"
+      end
+
+      def self.storage_key(service_id, username, attribute)
+        "service:#{service_id}/user:#{username}/#{attribute}"
+      end
 
       def to_hash
         {
@@ -139,13 +114,6 @@ module ThreeScale
         self.class.storage_key(service_id, username, attribute)
       end
 
-      def self.load_or_create!(service, user_id)
-        key = Memoizer.build_key(self, :load_or_create!, service.id, user_id)
-        Memoizer.memoize_block(key) do
-          super service, user_id
-        end
-      end
-
       def metric_names
         @metric_names ||= {}
       end
@@ -163,6 +131,22 @@ module ThreeScale
       end
 
       private
+
+      def self.clear_cache(service_id, user_id)
+        key = Memoizer.build_key(self, :load_or_create!, service_id, user_id)
+        Memoizer.clear key
+      end
+
+      def self.validate_attributes(attributes)
+        raise UserRequiresUsername if attributes[:username].nil?
+        raise UserRequiresServiceId if attributes[:service_id].nil?
+        service = Service.load_by_id(attributes[:service_id])
+        raise UserRequiresValidService if service.nil?
+        attributes[:plan_id] ||= service.default_user_plan_id
+        attributes[:plan_name] ||= service.default_user_plan_name
+        raise UserRequiresDefinedPlan if attributes[:plan_id].nil? || attributes[:plan_name].nil?
+        attributes[:state] ||= "active"
+      end
 
       # Username is used as a unique user ID
       def user_id
