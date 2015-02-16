f482111160e968a2fc5462dee7458bea0f8edd10
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-02-16 16:12:36 +0100

    backend: import Storable from Core

---
+++ lib/3scale/backend/storable.rb
@@ -0,0 +1,30 @@
+module ThreeScale
+  module Backend
+    # Mix this into objects that should be storable in the storage.
+    module Storable
+      include StorageKeyHelpers
+
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      def initialize(attributes = {})
+        attributes.each do |key, value|
+          send("#{key}=", value)
+        end
+      end
+
+      def storage
+        self.class.storage
+      end
+
+      module ClassMethods
+        include StorageKeyHelpers
+
+        def storage
+          Storage.instance
+        end
+      end
+    end
+  end
+end
