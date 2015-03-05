262f6da821e0d5f1e80ada79d55c0dfd2a3d2d61
Toni Reina <areina0@gmail.com>
2015-03-05 16:16:55 +0000

    Convert TransactionStorage module to a class
    It was a module and it makes no sense because it is not included in any
    class.

--- lib/3scale/backend/transaction_storage.rb
+++ lib/3scale/backend/transaction_storage.rb
@@ -1,40 +1,41 @@
 module ThreeScale
   module Backend
-    module TransactionStorage
-      include StorageHelpers
-      extend self
-
+    class TransactionStorage
       LIMIT = 50
 
-      def store_all(transactions)
-        transactions.each_slice(PIPELINED_SLICE_SIZE) do |slice|
-          storage.pipelined do
-            slice.each do |transaction|
-              store(transaction)
+      class << self
+        include StorageHelpers
+
+        def store_all(transactions)
+          transactions.each_slice(PIPELINED_SLICE_SIZE) do |slice|
+            storage.pipelined do
+              slice.each do |transaction|
+                store(transaction)
+              end
             end
           end
         end
-      end
 
-      def store(transaction)
-        key = queue_key(transaction.service_id)
+        def store(transaction)
+          key = queue_key(transaction.service_id)
 
-        storage.lpush(key,
-                      encode(application_id: transaction.application_id,
-                             usage:          transaction.usage,
-                             timestamp:      transaction.timestamp))
-        storage.ltrim(key, 0, LIMIT - 1)
-      end
+          storage.lpush(key,
+                        encode(application_id: transaction.application_id,
+                               usage:          transaction.usage,
+                               timestamp:      transaction.timestamp))
+          storage.ltrim(key, 0, LIMIT - 1)
+        end
 
-      def list(service_id)
-        raw_items = storage.lrange(queue_key(service_id), 0, -1)
-        raw_items.map(&method(:decode))
-      end
+        def list(service_id)
+          raw_items = storage.lrange(queue_key(service_id), 0, -1)
+          raw_items.map(&method(:decode))
+        end
 
-      private
+        private
 
-      def queue_key(service_id)
-        "transactions/service_id:#{service_id}"
+        def queue_key(service_id)
+          "transactions/service_id:#{service_id}"
+        end
       end
     end
   end
--- test/unit/transaction_storage_test.rb
+++ test/unit/transaction_storage_test.rb
@@ -12,6 +12,14 @@ class TransactionStorageTest < Test::Unit::TestCase
     @metric_id      = next_id
   end
 
+  def transaction(attrs = {})
+    default_attrs = {
+      service_id:     @service_id,
+      application_id: @application_id,
+    }
+    Transaction.new(default_attrs.merge(attrs))
+  end
+
   test '#store_all stores transactions to the storage' do
     service_id_one = @service_id
     service_id_two = next_id
@@ -22,22 +30,24 @@ class TransactionStorageTest < Test::Unit::TestCase
     metric_id_one = @metric_id
     metric_id_two = next_id
 
-    TransactionStorage.store_all([{:service_id     => service_id_one,
-                                   :application_id => application_id_one,
-                                   :usage          => {metric_id_one => 1},
-                                   :timestamp      => Time.utc(2010, 9, 10, 17, 4)},
-                                  {:service_id     => service_id_two,
-                                   :application_id => application_id_two,
-                                   :usage          => {metric_id_two => 2},
-                                   :timestamp      => Time.utc(2010, 9, 10, 17, 10)}])
+    TransactionStorage.store_all([
+      transaction(service_id: service_id_one,
+                  application_id: application_id_one,
+                  usage: { metric_id_one => 1 },
+                  timestamp: Time.utc(2010, 9, 10, 17, 4)),
+      transaction(service_id: service_id_two,
+                  application_id: application_id_two,
+                  usage: { metric_id_two => 2 },
+                  timestamp: Time.utc(2010, 9, 10, 17, 10)),
+    ])
 
     # Service one
     transactions = @storage.lrange("transactions/service_id:#{service_id_one}", 0, -1)
     assert_equal 1, transactions.size
 
-    expected = {'application_id' => application_id_one,
-                'usage'          => {metric_id_one => 1},
-                'timestamp'      => '2010-09-10 17:04:00 UTC' }
+    expected = { 'application_id' => application_id_one,
+                 'usage'          => { metric_id_one => 1 },
+                 'timestamp'      => '2010-09-10 17:04:00 UTC' }
 
     assert_equal expected, Yajl::Parser.parse(transactions[0])
 
@@ -45,9 +55,9 @@ class TransactionStorageTest < Test::Unit::TestCase
     transactions = @storage.lrange("transactions/service_id:#{service_id_two}", 0, -1)
     assert_equal 1, transactions.size
 
-    expected = {'application_id' => application_id_two,
-                'usage'          => {metric_id_two => 2},
-                'timestamp'      => '2010-09-10 17:10:00 UTC' }
+    expected = { 'application_id' => application_id_two,
+                 'usage'          => { metric_id_two => 2 },
+                 'timestamp'      => '2010-09-10 17:10:00 UTC' }
 
     assert_equal expected, Yajl::Parser.parse(transactions[0])
   end
@@ -57,51 +67,59 @@ class TransactionStorageTest < Test::Unit::TestCase
     application_id_two = next_id
 
     @storage.lpush("transactions/service_id:#{@service_id}",
-                   Yajl::Encoder.encode(:application_id => application_id_one,
-                                        :usage          => {@metric_id => 1},
-                                        :timestamp      => '2010-09-10 11:00:00 UTC'))
+                   Yajl::Encoder.encode(application_id: application_id_one,
+                                        usage:          { @metric_id => 1 },
+                                        timestamp:      '2010-09-10 11:00:00 UTC'))
 
     @storage.lpush("transactions/service_id:#{@service_id}",
-                   Yajl::Encoder.encode(:application_id => application_id_two,
-                                        :usage          => {@metric_id => 2},
-                                        :timestamp      => '2010-09-10 11:02:00 UTC'))
+                   Yajl::Encoder.encode(application_id: application_id_two,
+                                        usage:          { @metric_id => 2 },
+                                        timestamp:      '2010-09-10 11:02:00 UTC'))
 
-    expected = [{:application_id => application_id_two,
-                 :usage          => {@metric_id => 2},
-                 :timestamp      => Time.utc(2010, 9, 10, 11, 2)},
-                {:application_id => application_id_one,
-                 :usage          => {@metric_id => 1},
-                 :timestamp      => Time.utc(2010, 9, 10, 11, 0)}]
+    expected = [
+      {
+        application_id: application_id_two,
+        usage:          { @metric_id => 2 },
+        timestamp:      Time.utc(2010, 9, 10, 11, 2),
+      },
+      {
+        application_id: application_id_one,
+        usage:          { @metric_id => 1 },
+        timestamp:      Time.utc(2010, 9, 10, 11, 0),
+      },
+    ]
 
     assert_equal expected, TransactionStorage.list(@service_id)
   end
 
   test 'list returns previously stored transactions' do
-    TransactionStorage.store(:service_id     => @service_id,
-                             :application_id => @application_id,
-                             :usage          => {@metric_id => 7},
-                             :timestamp      => Time.utc(2010, 9, 10, 17, 29))
+    TransactionStorage.store(transaction(
+                               usage:     { @metric_id => 7 },
+                               timestamp: Time.utc(2010, 9, 10, 17, 29)),
+                            )
 
     transactions = TransactionStorage.list(@service_id)
     assert_equal 1, transactions.size
 
-    expected = {:application_id => @application_id,
-                :usage          => {@metric_id => 7},
-                :timestamp      => Time.utc(2010, 9, 10, 17, 29)}
+    expected = {
+      application_id: @application_id,
+      usage:          { @metric_id => 7 },
+      timestamp:      Time.utc(2010, 9, 10, 17, 29),
+    }
 
     assert_equal expected, transactions[0]
   end
 
   test 'list returns the transactions in reverse-insertion order' do
-    TransactionStorage.store(:service_id     => @service_id,
-                             :application_id => @application_id,
-                             :usage          => {@metric_id => 1},
-                             :timestamp      => Time.utc(2010, 9, 10, 17, 31))
+    TransactionStorage.store(transaction(
+                               usage:     { @metric_id => 1 },
+                               timestamp: Time.utc(2010, 9, 10, 17, 31)),
+                            )
 
-    TransactionStorage.store(:service_id     => @service_id,
-                             :application_id => @application_id,
-                             :usage          => {@metric_id => 2},
-                             :timestamp      => Time.utc(2010, 9, 10, 17, 32))
+    TransactionStorage.store(transaction(
+                               usage:     { @metric_id => 2 },
+                               timestamp: Time.utc(2010, 9, 10, 17, 32)),
+                            )
 
     transactions = TransactionStorage.list(@service_id)
     assert_equal 2, transactions[0][:usage][@metric_id]
@@ -110,21 +128,21 @@ class TransactionStorageTest < Test::Unit::TestCase
 
   test 'keeps at most 50 transactions in the storage' do
     60.times do
-      TransactionStorage.store(:service_id     => @service_id,
-                               :application_id => @application_id,
-                               :usage          => {@metric_id => 1},
-                               :timestamp      => Time.now.getutc)
+      TransactionStorage.store(transaction(
+                                 usage:     { @metric_id => 1 },
+                                 timestamp: Time.now.getutc),
+                              )
     end
 
     assert_equal 50, @storage.llen("transactions/service_id:#{@service_id}")
   end
-  
+
   test 'when more than 50 transactions are in the storage, the oldest ones are discarded' do
     60.times do |index|
-      TransactionStorage.store(:service_id     => @service_id,
-                               :application_id => @application_id,
-                               :usage          => {@metric_id => index},
-                               :timestamp      => Time.now.getutc)
+      TransactionStorage.store(transaction(
+                                 usage:     { @metric_id => index },
+                                 timestamp: Time.now.getutc),
+                              )
     end
 
     assert_equal 59, TransactionStorage.list(@service_id).first[:usage][@metric_id]
