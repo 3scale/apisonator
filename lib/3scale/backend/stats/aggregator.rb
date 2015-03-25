27a6177ef1439f6dc7c266f8acd7fc4efebccca1
Raimon Grau <raimon@3scale.net>
2015-03-25 12:18:39 +0000

    Split usage and response_code aggregators.
    Aggregator calls Aggregators::Usage and Aggregators::ResponseCode to
    perform the different kinds of aggregations.

--- lib/3scale/backend/stats/aggregator.rb
+++ lib/3scale/backend/stats/aggregator.rb
@@ -4,7 +4,8 @@ require '3scale/backend/stats/keys'
 require '3scale/backend/stats/replicate_job'
 require '3scale/backend/application_events'
 require '3scale/backend/transaction'
-require '3scale/backend/stats/response_code_aggregator'
+require '3scale/backend/stats/aggregators/response_code'
+require '3scale/backend/stats/aggregators/usage'
 
 module ThreeScale
   module Backend
@@ -14,10 +15,6 @@ module ThreeScale
           include Backend::StorageKeyHelpers
           include Configurable
           include Keys
-
-          GRANULARITY_EXPIRATION_TIME = {
-            minute: 180,
-          }
 
           attr_accessor :prior_bucket
 
@@ -53,9 +50,8 @@ module ThreeScale
             transactions.each_slice(PIPELINED_SLICE_SIZE) do |slice|
               storage.pipelined do
                 slice.each do |transaction|
-                  aggregate_usage(transaction, bucket)
+                  aggregate_all(transaction, bucket)
 
-                  aggregate_response_codes(transaction, bucket) if transaction.response_code
                   touched_apps.merge!(touched_relation(:application, transaction))
                   next unless transaction.user_id
                   touched_users.merge!(touched_relation(:user, transaction))
@@ -66,58 +62,9 @@ module ThreeScale
             { applications: touched_apps, users: touched_users }
           end
 
-          # transaction has a response_code
-          def aggregate_response_codes(transaction, bucket)
-            response_code = transaction.response_code
-            return unless response_code
-            rca = ResponseCodeAggregator.new
-            cmd = :incrby
-            value = 1
-            keys_with_multiple_rc = rca.aggregate(transaction)
-            keys_with_multiple_rc.each do |keys|
-              aggregate_values(value, transaction.timestamp, keys, cmd, bucket)
-            end
-          end
-
-          # Aggregates the usage of a transaction. If a bucket time is specified,
-          # all new or updated stats keys will be stored in a Redis Set with a name
-          # composed by 'keys_changed' + bucket.
-          #
-          # @param [Transaction] transaction
-          # @param [String, Nil] bucket
-          def aggregate_usage(transaction, bucket = nil)
-            bucket_key = Keys.changed_keys_bucket_key(bucket) if bucket
-
-            transaction.usage.each do |metric_id, raw_value|
-              metric_keys = Keys.transaction_metric_keys(transaction, metric_id)
-              cmd         = storage_cmd(raw_value)
-              value       = parse_usage_value(raw_value)
-
-              aggregate_values(value, transaction.timestamp, metric_keys, cmd, bucket_key)
-            end
-          end
-
-          # Aggregates a value in a timestamp for all given keys using a specific
-          # Redis command to store them. If a bucket_key is specified, each key will
-          # be added to a Redis Set with that name.
-          #
-          # @param [Integer] value
-          # @param [Time] timestamp
-          # @param [Array] keys  is an array of {(service|application) => "key"}
-          # @param [Symbol] cmd
-          # @param [String, Nil] bucket_key
-          def aggregate_values(value, timestamp, keys, cmd, bucket_key = nil)
-            granularities = [:eternity, :month, :week, :day, :hour]
-            keys.each do |metric_type, prefix_key|
-              granularities += [:year, :minute] unless metric_type == :service
-
-              granularities.each do |granularity|
-                key = counter_key(prefix_key, granularity, timestamp)
-                expire_time = expire_time_for_granularity(granularity)
-
-                store_key(cmd, key, value, expire_time)
-                store_in_changed_keys(key, bucket_key)
-              end
+          def aggregate_all(transaction, bucket)
+            [Aggregators::ResponseCode, Aggregators::Usage].each do |aggregator|
+              aggregator.new.aggregate(transaction, bucket)
             end
           end
 
@@ -141,46 +88,13 @@ module ThreeScale
             @stats_bucket_size ||= (configuration.stats.bucket_size || 5)
           end
 
-          def storage
-            Backend::Storage.instance
-          end
-
-          # Return Redis command depending on raw_value.
-          # If raw_value is a string with a '#' in the beginning, it returns 'set'.
-          # Else, it returns 'incrby'.
-          #
-          # @param [String] raw_value
-          # @return [Symbol] the Redis command
-          def storage_cmd(raw_value)
-            Helpers.get_value_of_set_if_exists(raw_value) ? :set : :incrby
-          end
-
-          # Parse 'raw_value' and return it as a integer.
-          # It take that raw_value can start with a '#' into consideration.
-          #
-          # @param [String] raw_value
-          # @return [Integer] the parsed value
-          def parse_usage_value(raw_value)
-            (Helpers.get_value_of_set_if_exists(raw_value) || raw_value).to_i
-          end
-
-          def store_key(cmd, key, value, expire_time = nil)
-            storage.send(cmd, key, value)
-            storage.expire(key, expire_time) if expire_time
-          end
-
-          def expire_time_for_granularity(granularity)
-            GRANULARITY_EXPIRATION_TIME[granularity]
-          end
-
-          def store_in_changed_keys(key, bucket_key = nil)
-            return unless bucket_key
-            storage.sadd(bucket_key, key)
-          end
-
           def enqueue_stats_job(bucket)
             return unless Storage.enabled?
             Resque.enqueue(ReplicateJob, bucket, Time.now.getutc.to_f)
+          end
+
+          def storage
+            Backend::Storage.instance
           end
 
           # Return a Hash with needed info to update the cached XMLs
