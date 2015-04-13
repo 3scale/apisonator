05cbd2a40c241fe716ddf73d66c6b48b0e600f86
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-04-13 17:01:40 +0200

    DRY transaction test fixtures

--- test/test_helpers/fixtures.rb
+++ test/test_helpers/fixtures.rb
@@ -147,23 +147,16 @@ module TestHelpers
       }
     end
 
-    def default_transaction(attrs = default_transaction_attrs)
-      Transaction.new(attrs)
+    def default_transaction attrs = {}
+      Transaction.new default_transaction_attrs.merge(attrs)
     end
 
     def transaction_with_set_value
-      attrs = default_transaction_attrs.merge(usage: { '3001' => '#665' })
-      Transaction.new(attrs)
+      default_transaction usage: { '3001' => '#665' }
     end
 
-    def transaction_with_response_code(code = 200)
-      transaction_with(response_code: code)
+    def transaction_with_response_code code = 200
+      default_transaction response_code: code
     end
-
-    def transaction_with(opts = {})
-      attrs = default_transaction_attrs.merge(opts)
-      Transaction.new(attrs)
-    end
-
   end
 end
