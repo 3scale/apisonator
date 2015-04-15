ad0d0152ef2fa05fb56921672bff7766b6da02cb
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-04-15 13:14:28 +0200

    logger: move ThreeScale::Backend::Logger to ThreeScale::Backend::Logger::Middleware

---
+++ lib/3scale/backend/logger/middleware.rb
@@ -0,0 +1,95 @@
+module ThreeScale
+  module Backend
+    class Logger
+      class Middleware
+        FORMAT = "%s - %s [%s] \"%s %s%s %s\" %d %s %s %s %s %s %s %s %s %s\n"
+        ERROR_FORMAT = "%s - %s [%s] \"%s %s%s %s\" %d \"%s\" %s %s\n"
+
+        def initialize(app, logger=nil)
+          @app = app
+          @logger = logger
+        end
+
+        def call(env)
+          began_at = Time.now
+          begin
+            status, header, body = @app.call(env)
+          rescue Exception => e
+            log_error(env, 500, e.message, began_at)
+            raise e
+          end
+          header = Rack::Utils::HeaderHash.new(header)
+          body = Rack::BodyProxy.new(body) { log(env, status, header, began_at) }
+          [status, header, body]
+        end
+
+        private
+
+        def log_error(env, status, error, began_at)
+          now = Time.now.getutc
+          qs  = extract_query_string(env)
+
+          logger = @logger || env['rack.errors']
+          logger.write ERROR_FORMAT % [
+            env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-",
+            env["REMOTE_USER"] || "-",
+            now.strftime("%d/%b/%Y %H:%M:%S %Z"),
+            env["REQUEST_METHOD"],
+            env["PATH_INFO"],
+            qs.empty? ? "" : "?" + qs.gsub("\n",""),
+            env["HTTP_VERSION"],
+            status.to_s[0..3],
+            error,
+            now - began_at,
+            env['HTTP_X_REQUEST_ID']]
+        end
+
+        def log(env, status, header, began_at)
+          now    = Time.now.getutc
+          qs     = extract_query_string(env)
+          length = extract_content_length(header)
+
+          logger = @logger || env['rack.errors']
+          logger.write FORMAT % [
+            env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-",
+            env["REMOTE_USER"] || "-",
+            now.strftime("%d/%b/%Y %H:%M:%S %Z"),
+            env["REQUEST_METHOD"],
+            env["PATH_INFO"],
+            qs.empty? ? "" : "?"+qs.gsub("\n",""),
+            env["HTTP_VERSION"],
+            status.to_s[0..3],
+            length,
+            now - began_at,
+            ThreeScale::Backend::Cache.stats[:last] || "-",
+            ThreeScale::Backend::Cache.stats[:count] || "-",
+            ThreeScale::Backend::Cache.stats[:hits] || "-",
+            ThreeScale::Backend::Memoizer.stats[:size] || "-",
+            ThreeScale::Backend::Memoizer.stats[:count] || "-",
+            ThreeScale::Backend::Memoizer.stats[:hits] || "-",
+            env['HTTP_X_REQUEST_ID']]
+        end
+
+        def extract_content_length(headers)
+          value = headers['Content-Length'] or return '-'
+          value.to_s == '0' ? '-' : value
+        end
+
+        def extract_query_string(env)
+          if env["REQUEST_METHOD"].to_s.upcase == "POST"
+            provider_key = Rack::Request.new(env).params["provider_key"]
+            qs = env["QUERY_STRING"].dup
+            unless provider_key.nil?
+              qs << "&" unless env["QUERY_STRING"].empty?
+              qs << "provider_key=#{provider_key}"
+            end
+          else
+            qs = env["QUERY_STRING"]
+          end
+
+          qs
+        end
+      end
+    end
+  end
+end
--- test/integration/logger_test.rb
+++ test/integration/logger_test.rb
@@ -5,13 +5,13 @@ class LoggerTest < Test::Unit::TestCase
 
   def app
     Rack::Builder.new {
-      use ThreeScale::Backend::Logger
+      use ThreeScale::Backend::Logger::Middleware
       run ThreeScale::Backend::Listener.new
     }.to_app
   end
 
   test 'log valid requests with request info and stats info' do
-    ThreeScale::Backend::Logger.any_instance.expects(:log).times(1)
+    ThreeScale::Backend::Logger::Middleware.any_instance.expects(:log).times(1)
 
     assert_nothing_raised do
       post '/transactions.xml?transactions[0]=foo2', provider_key: 'foo'
@@ -22,7 +22,7 @@ class LoggerTest < Test::Unit::TestCase
     redis = Backend::Storage.any_instance
     redis.expects(:get).raises(TimeoutError)
 
-    ThreeScale::Backend::Logger.any_instance.expects(:log_error).times(1)
+    ThreeScale::Backend::Logger::Middleware.any_instance.expects(:log_error).times(1)
 
     assert_raise TimeoutError do
       post '/transactions.xml?transactions[0]=foo2', provider_key: 'foo'
