765e718f0611c11ae03833ef8b5129fb3f423f4b
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-06-26 17:07:56 +0200

    Add acceptance spec for setting the bucket

--- spec/acceptance/api/internal/services_api_spec.rb
+++ spec/acceptance/api/internal/services_api_spec.rb
@@ -147,4 +147,16 @@ resource "Services (prefix: /services)" do
     end
   end
 
+  post '/services/:id/logs_bucket' do
+    parameter :id, 'Service ID', required: true
+
+    let(:raw_post) { params.to_json }
+
+    example_request 'Setting the request log bucket for a service', id: 1001 do
+      status.should == 200
+      ThreeScale::Backend::CubertServiceManagementUseCase.new(1001).enabled?.
+        should == true
+    end
+  end
+
 end
