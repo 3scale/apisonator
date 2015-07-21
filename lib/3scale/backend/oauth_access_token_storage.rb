ab6043c61fc385c2e5aae9a488d0862afcce2b62
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-07-21 15:01:42 +0200

    oauth_access_token_storage: use metaclass for module class methods

--- lib/3scale/backend/oauth_access_token_storage.rb
+++ lib/3scale/backend/oauth_access_token_storage.rb
@@ -16,74 +16,78 @@ module ThreeScale
 
       MAXIMUM_TOKEN_SIZE = 1024
 
-      # Creates OAuth Access Token association with the Application.
-      #
-      # Returns false in case of invalid invalid params (negative TTL
-      # or invalid token).
-      #
-      def self.create(service_id, app_id, token, ttl = nil)
-        ##return false unless token =~ /\A(\w|-)+\Z/
-        ##anything can go on an access token
-        return false if token.nil? || token.empty? || !token.is_a?(String) || token.size > MAXIMUM_TOKEN_SIZE
+      class << self
 
-        key = token_key(service_id, token)
+        # Creates OAuth Access Token association with the Application.
+        #
+        # Returns false in case of invalid invalid params (negative TTL
+        # or invalid token).
+        #
+        def create(service_id, app_id, token, ttl = nil)
+          ##return false unless token =~ /\A(\w|-)+\Z/
+          ##anything can go on an access token
+          return false if token.nil? || token.empty? || !token.is_a?(String) || token.size > MAXIMUM_TOKEN_SIZE
 
-        raise AccessTokenAlreadyExists.new(token) unless storage.get(key).nil?
+          key = token_key(service_id, token)
 
-        if ttl.nil?
-          storage.set(key, app_id)
-        else
-          ttl = ttl.to_i
-          return false if ttl <= 0
+          raise AccessTokenAlreadyExists.new(token) unless storage.get(key).nil?
 
-          storage.setex(key, ttl, app_id)
-        end
-
-        storage.sadd(token_set_key(service_id, app_id), token)
-        true
-      end
-
-      def self.delete(service_id, token)
-        key = token_key(service_id, token)
-        app_id = storage.get key
-        storage.pipelined do
-          storage.del key
-          storage.srem(token_set_key(service_id, app_id), token)
-        end
-      end
-
-      def self.all_by_service_and_app(service_id, app_id)
-        tokens = storage.smembers(token_set_key(service_id, app_id))
-        keys = tokens.map { |t| token_key(service_id, t) }
-        applications = keys.empty? ? [] : storage.mget(keys)
-        set_key = token_set_key(service_id, app_id)
-
-        result = tokens.map.with_index do |token,i|
-          if applications[i].nil?
-            # remove expired tokens from the set
-            storage.srem(set_key, token)
-            nil
+          if ttl.nil?
+            storage.set(key, app_id)
           else
-            ttl = storage.ttl(keys[i])
-            OAuthAccessToken.new(token, ttl)
+            ttl = ttl.to_i
+            return false if ttl <= 0
+
+            storage.setex(key, ttl, app_id)
+          end
+
+          storage.sadd(token_set_key(service_id, app_id), token)
+          true
+        end
+
+        def delete(service_id, token)
+          key = token_key(service_id, token)
+          app_id = storage.get key
+          storage.pipelined do
+            storage.del key
+            storage.srem(token_set_key(service_id, app_id), token)
           end
         end
 
-        result.compact
-      end
+        def all_by_service_and_app(service_id, app_id)
+          tokens = storage.smembers(token_set_key(service_id, app_id))
+          keys = tokens.map { |t| token_key(service_id, t) }
+          applications = keys.empty? ? [] : storage.mget(keys)
+          set_key = token_set_key(service_id, app_id)
 
-      def self.get_app_id(service_id, token)
-        storage.get(token_key(service_id, token))
-      end
+          result = tokens.map.with_index do |token,i|
+            if applications[i].nil?
+              # remove expired tokens from the set
+              storage.srem(set_key, token)
+              nil
+            else
+              ttl = storage.ttl(keys[i])
+              OAuthAccessToken.new(token, ttl)
+            end
+          end
 
-      private
+          result.compact
+        end
 
-      def self.token_key(service_id, token)
-        "oauth_access_tokens/service:#{service_id}/#{token}"
-      end
+        def get_app_id(service_id, token)
+          storage.get(token_key(service_id, token))
+        end
 
-      def self.token_set_key(service_id, app_id)
-        "oauth_access_tokens/service:#{service_id}/app:#{app_id}/"
+        private
+
+        def token_key(service_id, token)
+          "oauth_access_tokens/service:#{service_id}/#{token}"
+        end
+
+        def token_set_key(service_id, app_id)
+          "oauth_access_tokens/service:#{service_id}/app:#{app_id}/"
+        end
+
       end
     end
   end
