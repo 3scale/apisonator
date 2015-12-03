7b04fbfd1c75cdb2ac9ce7154ef166b28af0c814
David Ortiz <z.david.ortiz@gmail.com>
2015-12-03 18:14:49 +0100

    Add stats parser
    We are using keys like this one to store stats in Redis:
    "stats/{service:1006}/cinstance:a60/metric:255/week:20151130"
    This parser accepts a key like that and its value, and
    constructs a Hash. That Hash is going to be what we are going
    to send to the Kinesis adapter that we will develop later.

---
+++ lib/3scale/backend/stats/stats_parser.rb
@@ -0,0 +1,112 @@
+module ThreeScale
+  module Backend
+    module Stats
+      class StatsParser
+        # This parser converts a stats key of Redis into a Hash
+        # This is an example of stats key that this parser converts:
+        # "stats/{service:1006}/cinstance:a60/metric:255/week:20151130"
+
+        # This class contains code stolen from Alex's script:
+        # /script/redis/stats_keys_2_csv
+        # We can think about unifying code later.
+
+        DATE_COLS = [
+            'year'.freeze,
+            'month'.freeze,
+            'day'.freeze,
+            'hour'.freeze,
+            'minute'.freeze,
+        ].freeze
+        private_constant :DATE_COLS
+
+        PERIODS = [
+            *DATE_COLS,
+            'week'.freeze,
+            'eternity'.freeze,
+        ].freeze
+        private_constant :PERIODS
+
+        NON_DATE_PERIODS = (PERIODS - DATE_COLS).freeze
+        private_constant :NON_DATE_PERIODS
+
+        ALL_COLUMNS = [
+            *DATE_COLS,
+            'period'.freeze,
+            'service'.freeze,
+            'cinstance'.freeze,
+            'uinstance'.freeze,
+            'metric'.freeze,
+            'response_code'.freeze,
+            'value'.freeze,
+        ].freeze
+        private_constant :ALL_COLUMNS
+
+        REQUIRED_COLS = [
+            *DATE_COLS,
+            'period'.freeze,
+            'service'.freeze,
+            'value'.freeze,
+        ].freeze
+        private_constant :REQUIRED_COLS
+
+        StatsKeyValueInvalid = Class.new(ThreeScale::Backend::Error)
+
+        class << self
+
+          def parse(stats_key, value)
+            key_value_to_hash(stats_key, value)
+          end
+
+          private
+
+          def key_value_to_hash(key,value)
+            key_value = ("\"#{key}\":\"#{value}\"")
+
+            # some keys have things like "field1:xxx/uinstance:N/A/field3:yyy" WTF.
+            key_value.gsub!(/:N\/A/, ':'.freeze)
+
+            h = Hash[str2ary(prepare_str_from(key_value))]
+
+            result = fix_dates_and_periods(h)
+
+            all_required_columns = REQUIRED_COLS.all? { |col| h.has_key?(col) }
+            no_extra_columns = (h.keys - ALL_COLUMNS).empty?
+
+            unless all_required_columns && no_extra_columns
+              raise StatsKeyValueInvalid, "Error parsing #{key_value}"
+            end
+
+            Hash[result.map{ |k, v| [k.to_sym, v] }]
+          end
+
+          def prepare_str_from(line)
+            _, key, _, val, *_ = line.split('"')
+            "#{key.gsub(/[\{\}]/, '')}/value:#{val}"
+          end
+
+          def str2ary(str)
+            str.split('/')[1..-1].map do |kv|
+              kv.split(':')
+            end
+          end
+
+          def fix_dates_and_periods(hash)
+            period = hash.keys.find { |k| PERIODS.include? k }
+            if period
+              hash['period'.freeze] = period
+              period_val = hash[period].dup
+              hash['year'.freeze] = period_val.slice! 0, 4
+              ['month'.freeze, 'day'.freeze, 'hour'.freeze, 'minute'.freeze].each do |p|
+                hash[p] = period_val.slice! 0, 2
+                hash[p] = nil if hash[p].empty?
+              end
+              NON_DATE_PERIODS.each { |ndp| hash.delete ndp }
+            end
+            hash
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/stats/stats_parser_spec.rb
@@ -0,0 +1,94 @@
+require_relative '../../spec_helper'
+require_relative '../../../lib/3scale/backend/stats/stats_parser'
+
+module ThreeScale
+  module Backend
+    module Stats
+      describe ThreeScale::Backend::Stats::StatsParser do
+        describe '.parse' do
+          let(:service) { '19' }
+          let(:cinstance) { '85fb' }
+          let(:uinstance) { '86fb' }
+          let(:metric) { '299' }
+          let(:period) { 'day' }
+          let(:time) { '20151202' }
+          let(:value) { '10' }
+
+          subject { StatsParser }
+
+          context 'with a key that contains all the required params' do
+            let(:key) do
+              "stats/{service:#{service}}/cinstance:#{cinstance}/metric:#{metric}/#{period}:#{time}"
+            end
+
+            it 'returns the correct hash' do
+              expect(subject.parse(key, value))
+                  .to eq({ service: service,
+                           cinstance: cinstance,
+                           metric: metric,
+                           period: period,
+                           year: time[0..3],
+                           month: time[4..5],
+                           day: time[6..7],
+                           hour: nil,
+                           minute: nil,
+                           value: value })
+            end
+          end
+
+          context 'with a key that does not contain all the required params' do
+            let(:key) do
+              "stats/{service:#{service}}"
+            end
+
+            it 'raises StatsKeyValueInvalid' do
+              expect { subject.parse(key, value) }
+                  .to raise_error(StatsParser::StatsKeyValueInvalid)
+            end
+          end
+
+          context 'with a key that contains non-expected params' do
+            let(:key) { 'stats/non_expected_param:3' }
+
+            it 'raises StatsKeyValueInvalid' do
+              expect { subject.parse(key, value) }
+                  .to raise_error(StatsParser::StatsKeyValueInvalid)
+            end
+          end
+
+          context 'with a key that contains an invalid period' do
+            let(:period) { 'invalid_period' }
+            let(:key) do
+              "stats/{service:#{service}}/cinstance:#{cinstance}/metric:#{metric}/#{period}:#{time}"
+            end
+
+            it 'raises StatsKeyValueInvalid' do
+              expect { subject.parse(key, value) }
+                  .to raise_error(StatsParser::StatsKeyValueInvalid)
+            end
+          end
+
+          context 'with a key that contains N/A' do
+            let(:key) do
+              "stats/{service:#{service}}/uinstance:N/A/metric:#{metric}/#{period}:#{time}"
+            end
+
+            it 'returns the correct hash, setting the param with N/A to nil' do
+              expect(subject.parse(key, value))
+                  .to eq({ service: service,
+                           uinstance: nil,
+                           metric: metric,
+                           period: period,
+                           year: time[0..3],
+                           month: time[4..5],
+                           day: time[6..7],
+                           hour: nil,
+                           minute: nil,
+                           value: value })
+            end
+          end
+        end
+      end
+    end
+  end
+end
