619234e196ad2861093deaf7c32eb49833b13e83
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-12-14 15:53:04 +0100

    test/integration/listener: test that we receive 400 Bad Request when we use big parameters

--- test/integration/listener_test.rb
+++ test/integration/listener_test.rb
@@ -6,6 +6,77 @@ class ListenerTest < Test::Unit::TestCase
   UnexpectedError = Class.new(RuntimeError)
   ExpectedError   = Class.new(Error)
 
+  TOOBIGSIZE_PARAM = Listener::MAX_PARAM_LENGTH + 1
+  TOOBIGSIZE_TOKEN = OAuthAccessTokenStorage::MAXIMUM_TOKEN_SIZE + 1
+
+  def test_big_params_limit_less_or_equal_big_token_limit
+    assert TOOBIGSIZE_PARAM <= TOOBIGSIZE_TOKEN
+  end
+
+  def test_big_params_in_known_parameter
+    get "/transactions/authorize.xml?provider_key=#{'x' * TOOBIGSIZE_PARAM}"
+    assert_too_big
+  end
+
+  def test_big_params_in_unknown_parameter
+    get "/transactions/authrep.xml?unknown_param=#{'x' * TOOBIGSIZE_PARAM}"
+    assert_too_big
+  end
+
+  def test_big_params_in_key_only
+    get "/transactions/authrep.xml?#{'x' * TOOBIGSIZE_PARAM}=1"
+    assert_too_big
+  end
+
+  def test_big_params_in_body
+    post '/transactions.xml', provider_key: '123', some_param: "#{'x' * TOOBIGSIZE_PARAM}"
+    assert_too_big
+    post '/transactions.xml', provider_key: '123', some_param: "#{'x' * (TOOBIGSIZE_PARAM - 1)}"
+    assert_not_too_big
+  end
+
+  def test_big_param_returns_error_message
+    get "/transactions/authrep.xml?whatever_param=#{'x' * TOOBIGSIZE_PARAM}"
+    doc = Nokogiri::XML(last_response.body)
+    error = doc.at('error:root')
+    assert_not_nil error
+    assert_equal 'params_too_big', error['code']
+    assert_equal 'At least one parameter or its value is too big', error.content
+  end
+
+  def test_big_params_request_with_oauth_access_token_storage
+    post "/services/#{'x' * TOOBIGSIZE_PARAM}/oauth_access_tokens.xml?token=123"
+    assert_too_big
+    post "/services/666/oauth_access_tokens.xml?token=#{'x' * TOOBIGSIZE_TOKEN}"
+    assert_too_big
+    post "/services/666/oauth_access_tokens.xml?token=#{'x' * (TOOBIGSIZE_TOKEN-1)}"
+    assert_not_too_big
+  end
+
+  def test_big_params_request_with_oauth_access_token_storage_parameter
+    get "/transactions/oauth_authorize.xml?provider_key=123&access_token=#{'x' * TOOBIGSIZE_TOKEN}"
+    assert_too_big
+    get "/transactions/oauth_authorize.xml?provider_key=123&access_token=#{'x' * (TOOBIGSIZE_TOKEN-1)}"
+    assert_not_too_big
+  end
+
+  def test_big_param_inside_hash_returns_400
+    get "/transactions/authorize.xml?bighash[x]=1&bighash[#{'x' * TOOBIGSIZE_PARAM}]=1"
+    assert_too_big
+    get "/transactions/authorize.xml?okhash[x]=1&okhash[#{'x' * (TOOBIGSIZE_PARAM - 1)}]=1"
+    assert_not_too_big
+    post '/transactions.xml', provider_key: '123',
+      transactions: {0 => {:app_id => '456', :usage => {"#{'x' * TOOBIGSIZE_PARAM}" => 1} }}
+    assert_too_big
+  end
+
+  def test_big_param_inside_array_returns_400
+    get "/transactions/authorize.xml?bigarray[]=x&bigarray[]=#{'x' * TOOBIGSIZE_PARAM}"
+    assert_too_big
+    get "/transactions/authorize.xml?okarray[]=x&okarray[]=#{'x' * (TOOBIGSIZE_PARAM - 1)}"
+    assert_not_too_big
+  end
+
   def test_on_invalid_path_responds_with_404
     post '/foo.html'
     assert_equal 404, last_response.status
