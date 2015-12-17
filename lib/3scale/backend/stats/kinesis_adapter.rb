26c9dbb776abc079fd272cab5501332de13d3b75
David Ortiz <z.david.ortiz@gmail.com>
2015-12-17 15:33:48 +0100

    KinesisAdapter: implement basic batching strategy
    To simplify thing for the proof of concept, we are going to use a
    basic batching strategy. This strategy is based on the number of
    events of each record and batch instead of the size in bytes.
    We will implement more complex batching strategies later.
    In a later commit, we will deal with the different types of errors
    that Kinesis can return.

--- lib/3scale/backend/stats/kinesis_adapter.rb
+++ lib/3scale/backend/stats/kinesis_adapter.rb
@@ -7,11 +7,18 @@ module ThreeScale
         # metric, period, time, value, etc. This means that the size of one
         # of our events is nowhere near 5KB. For that reason, we need to make
         # sure that we send many events in each record.
-        # The max size for each record is 1000KB.
-        # We will try to optimize the batching process later.
+        # The max size for each record is 1000KB. In each record batch, Kinesis
+        # accepts a maximum of 4MB.
+        #
+        # We will try to optimize the batching process later. For now, I will
+        # just put 300 events in each record. And batches of 5 records max.
+        # We will also handle Kinesis errors later.
 
-        MAX_RECORD_SIZE_BYTES = 1000*1024
-        private_constant :MAX_RECORD_SIZE_BYTES
+        EVENTS_PER_RECORD = 300
+        private_constant :EVENTS_PER_RECORD
+
+        MAX_RECORDS_PER_BATCH = 5
+        private_constant :MAX_RECORDS_PER_BATCH
 
         def initialize(stream_name, kinesis_client)
           @stream_name = stream_name
@@ -22,12 +29,16 @@ module ThreeScale
         def send_events(events)
           self.pending_events += events
 
-          # Keep saving events unless we can fill one record at least
-          if size_bytes(pending_events) >= MAX_RECORD_SIZE_BYTES
+          # Batch events until we can fill at least one record
+          if pending_events.size >= EVENTS_PER_RECORD
+            events_to_send = pending_events.take(EVENTS_PER_RECORD*MAX_RECORDS_PER_BATCH)
+            events_not_to_send = pending_events[EVENTS_PER_RECORD*MAX_RECORDS_PER_BATCH..-1] || []
+
             kinesis_client.put_record_batch(
                 { delivery_stream_name: stream_name,
-                  records: events_to_kinesis_records(pending_events) })
-            self.pending_events = []
+                  records: events_to_kinesis_records(events_to_send) })
+
+            self.pending_events = events_not_to_send
           end
         end
 
@@ -39,29 +50,9 @@ module ThreeScale
         def events_to_kinesis_records(events)
           # Record format expected by Kinesis:
           # [{ data: "data_event_group_1" }, { data: "data_event_group_2" }]
-          group_events(events).map do |events_group|
-            { data: events_group.to_json }
+          events.each_slice(EVENTS_PER_RECORD).map do |events_slice|
+            { data: events_slice.to_json }
           end
-        end
-
-        def group_events(events)
-          result = []
-          current_group = []
-
-          events.each do |event|
-            if size_bytes(current_group + [event]) < MAX_RECORD_SIZE_BYTES
-              current_group << event
-            else
-              result << current_group
-              current_group = [event]
-            end
-          end
-
-          result << current_group
-        end
-
-        def size_bytes(events)
-          events.to_json.bytesize
         end
       end
     end
--- spec/unit/stats/kinesis_adapter_spec.rb
+++ spec/unit/stats/kinesis_adapter_spec.rb
@@ -7,43 +7,94 @@ module ThreeScale
       describe KinesisAdapter do
         let(:kinesis_client) { double }
         let(:stream_name) { 'backend_stream' }
+        let(:events_per_record) { described_class.const_get(:EVENTS_PER_RECORD) }
+        let(:max_records_per_batch) { described_class.const_get(:MAX_RECORDS_PER_BATCH) }
+        let(:event) { { service: 's', metric: 'm', period: 'year', year: '2015' } }
 
         subject { described_class.new(stream_name, kinesis_client) }
 
         describe '#send_events' do
-          let(:record_size) { described_class.const_get(:MAX_RECORD_SIZE_BYTES) }
+          context 'the number of events is smaller than the number of events per record' do
+            let(:events) { Array.new(events_per_record - 1, event) }
 
-          context 'size of events is bigger than the size of a single record' do
-            # Prepare the data so we get 2 groups of records: the first with 2
-            # events and the second with 1.
+            before { expect(kinesis_client).not_to receive(:put_record_batch) }
 
-            # 2 services of 'record_size' would not fit in a record, because of
-            # the json overhead. That is why I multiply by a constant.
-            let(:event) { { service: '0'*((record_size/2)*0.8) } }
-            let(:events) { Array.new(3, event) }
+            it 'does not send the events to Kinesis' do
+              subject.send_events(events)
+            end
+
+            it 'adds the events to the array of pending events' do
+              subject.send_events(events)
+              expect(subject.send(:pending_events)).to eq events
+            end
+          end
+
+          context 'the number of events is enough to fill a record and can be send in 1 batch' do
+            let(:events) { Array.new(events_per_record, event) }
 
             before do
               expect(kinesis_client)
                   .to receive(:put_record_batch)
                           .with({ delivery_stream_name: stream_name,
-                                  records: [{ data: Array.new(2, event).to_json },
-                                            { data: [event].to_json }] })
+                                  records: [{ data: events.to_json }] })
             end
 
-            it 'sends the events in well-constructed records' do
+            it 'sends the events to Kinesis' do
               subject.send_events(events)
+            end
+
+            it 'pending events is empty' do
+              subject.send_events(events)
+              expect(subject.send(:pending_events)).to be_empty
             end
           end
 
-          context 'size of events is smaller than the size of a single record' do
-            let(:events) do
-              [{ service: 's', metric: 'm', period: 'year', year: '2015' }]
+          context 'the number of events fills several records but can be send in 1 batch' do
+            let(:records) { 2 } # Assuming that a batch can contain at least 2 records
+            let(:events) { Array.new(records*events_per_record, event) }
+            let(:kinesis_records) do
+              Array.new(records, { data: Array.new(events_per_record, event).to_json })
             end
 
-            before { expect(kinesis_client).not_to receive(:put_record_batch) }
+            before do
+              expect(kinesis_client)
+                  .to receive(:put_record_batch)
+                          .with({ delivery_stream_name: stream_name,
+                                  records: kinesis_records })
+            end
 
-            it 'does not send the events' do
+            it 'sends the events to Kinesis' do
               subject.send_events(events)
+            end
+
+            it 'pending events is empty' do
+              subject.send_events(events)
+              expect(subject.send(:pending_events)).to be_empty
+            end
+          end
+
+          context 'the number of events is too big to be sent in just one batch' do
+            let(:records) { max_records_per_batch + 1 }
+            let(:events) { Array.new(records*events_per_record, event) }
+            let(:kinesis_records) do
+              Array.new(max_records_per_batch,
+                        { data: Array.new(events_per_record, event).to_json })
+            end
+
+            before do
+              expect(kinesis_client)
+                  .to receive(:put_record_batch)
+                          .with({ delivery_stream_name: stream_name,
+                                  records: kinesis_records })
+            end
+
+            it 'sends a batch to Kinesis' do
+              subject.send_events(events)
+            end
+
+            it 'pending events includes the events that did not fit in the batch' do
+              subject.send_events(events)
+              expect(subject.send(:pending_events)).to eq Array.new(events_per_record, event)
             end
           end
         end
