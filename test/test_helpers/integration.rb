301cfe1452ba0a5b381b7a00b11dbfe05db9dddb
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-12-23 17:37:44 +0100

    test/helpers: introduce helpers to test for too_big parameters

--- test/test_helpers/integration.rb
+++ test/test_helpers/integration.rb
@@ -27,5 +27,20 @@ module TestHelpers
       assert_equal options[:code],    node['code'] if options[:code]
       assert_equal options[:message], node.content if options[:message]
     end
+
+    # helpers for assertions about big parameters
+    def response_too_big_status(response = last_response)
+      response.status == 400 &&
+        (err = Nokogiri::XML(response.body).at('error:root')) &&
+        err['code'] == 'params_too_big'
+    end
+
+    def assert_too_big(response = last_response)
+      assert response_too_big_status(response)
+    end
+
+    def assert_not_too_big(response = last_response)
+      refute response_too_big_status(response)
+    end
   end
 end
