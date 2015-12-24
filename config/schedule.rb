88df1e007171313aab6f8e2a6ed3117880e19d02
David Ortiz <z.david.ortiz@gmail.com>
2015-12-24 16:14:45 +0100

    Add scheduler for whenever job to send events to Kinesis

---
+++ config/schedule.rb
@@ -0,0 +1,5 @@
+job_type :backend_rake, 'cd :path && /usr/local/bin/bundle exec rake2.2 :task --silent'
+
+every 2.minutes do
+  backend_rake 'stats:kinesis:send_to_kinesis', path: '/home/bender/backend'
+end
