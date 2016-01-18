f594558d8365b975dc716479ffd7069af41104f4
David Ortiz <z.david.ortiz@gmail.com>
2016-01-18 18:21:16 +0100

    tasks: delete influxdb task

--- lib/3scale/backend/stats/tasks.rb
+++ lib/3scale/backend/stats/tasks.rb
@@ -11,23 +11,6 @@ module ThreeScale
 
         module_function
 
-        def check_values(service_id, application_id, metric_id, timestamp)
-          granularities = [:hour, :day, :week, :month, :year]
-          results = { redis: {}, influxdb: {} }
-
-          service_prefix            = service_key_prefix(service_id)
-          application_prefix        = application_key_prefix(service_prefix, application_id)
-          application_metric_prefix = metric_key_prefix(application_prefix, metric_id)
-
-          granularities.each do |gra|
-            redis_key               = counter_key(application_metric_prefix, gra, timestamp)
-            results[:redis][gra]    = storage.get(redis_key).to_i
-            results[:influxdb][gra] = storage_stats.get(service_id, metric_id, gra, timestamp, application: application_id).to_i
-          end
-
-          results
-        end
-
         def delete_all_buckets_and_keys_only_as_rake!(options = {})
           Storage.disable!
 
@@ -45,10 +28,6 @@ module ThreeScale
 
         def self.storage
           Backend::Storage.instance
-        end
-
-        def self.storage_stats
-          Storage.instance
         end
       end
     end
