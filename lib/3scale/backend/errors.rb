fba7cf3307564ca9a2334b273c5ca4a62084bfbc
David Ortiz <z.david.ortiz@gmail.com>
2016-01-26 15:13:05 +0100

    errors: add errors for transaction timestamps outside valid range

--- lib/3scale/backend/errors.rb
+++ lib/3scale/backend/errors.rb
@@ -194,9 +194,15 @@ module ThreeScale
       end
     end
 
-    class ReportTimestampNotWithinRange < Error
+    class TransactionTimestampTooOld < Error
       def initialize(max_seconds)
-        super %(report jobs cannot update metrics older than #{max_seconds} seconds)
+        super %(reporting transactions older than #{max_seconds} seconds is not allowed)
+      end
+    end
+
+    class TransactionTimestampTooNew < Error
+      def initialize(max_seconds)
+        super %(reporting transactions more than #{max_seconds} seconds in the future is not allowed)
       end
     end
 
