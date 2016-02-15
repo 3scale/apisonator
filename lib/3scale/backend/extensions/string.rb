9fa1af5b74f256a031f966f4426da000af61c67e
David Ortiz <z.david.ortiz@gmail.com>
2016-02-15 13:17:40 +0100

    Delete unused methods from string extension
    escape_whitespaces() and unescape_whitespaces() are not used.

--- lib/3scale/backend/extensions/string.rb
+++ lib/3scale/backend/extensions/string.rb
@@ -2,26 +2,6 @@ module ThreeScale
   module Backend
     module Extensions
       module String
-        def escape_whitespaces
-          gsub("_", "\\_").            # escape underscores
-          gsub(" ", "_").              # replace spaces with underscores
-
-          # This is bit counterintuitive, but it's right. I need to escape the backslashes
-          # twice - first as normal double quoted string, second because the replacement
-          # can contain backreferences, so I have to escape those too.
-          gsub("\\n", "\\\\\\n").      # escape escaped newlines
-
-          gsub("\n", "\\n")            # escape newlines
-        end
-
-        def unescape_whitespaces
-          gsub(/(?<!\\)_/, " ").       # replace not-escaped underscores with spaces
-          gsub("\\_", "_").            # unescape escaped underscores
-          gsub(/(?<!\\)\\n/, "\n").    # unescape escaped newlines
-          gsub("\\\\n", "\\n")         # unescape doubly escaped newlines
-        end
-
-
         def blank?
           self !~ /\S/
         end
