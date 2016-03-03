6c1e2c486aa72a493882ec4d086766dab3f1b713
David Ortiz <z.david.ortiz@gmail.com>
2016-03-03 18:26:27 +0100

    whenever scheduler: add redshift importing

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,7 @@ job_type :backend_rake, 'cd :path && /usr/local/bin/bundle exec rake2.2 :task --
 every 2.minutes do
   backend_rake 'stats:kinesis:send', path: '/home/bender/backend'
 end
+
+every 1.hour do
+  backend_rake 'stats:redshift:import', path: '/home/bender/backend'
+end
