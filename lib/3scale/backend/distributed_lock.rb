05b302544051bf89242fe5e3bb1c2a2aef72364a
David Ortiz <z.david.ortiz@gmail.com>
2016-04-18 16:03:34 +0200

    distributed lock: move explanation and adapt it to be generic

--- lib/3scale/backend/distributed_lock.rb
+++ lib/3scale/backend/distributed_lock.rb
@@ -1,5 +1,28 @@
 module ThreeScale
   module Backend
+
+    # This class uses Redis to implement a distributed lock.
+    #
+    # To implement the distributed lock, we use the Redis operation 'set nx'.
+    # The locking algorithm is detailed here: http://redis.io/topics/distlock
+    # Basically, every time that we want to use the lock, we generate a random
+    # number and set a key in Redis with that random number if its current
+    # value is null. If we could set the value, it means that we could get the
+    # lock. To release it, we just need to set to delete the same key.
+    #
+    # The random number that we use is the current unix epoch in ms. This does
+    # not ensure 100% that the locking algorithm works correctly. Also, the
+    # implementation used in this class does not ensure a correct behavior if
+    # for some reason one of the Redis masters fails and a slave takes its
+    # place. However, in the jobs where we are currently using a distributed
+    # lock, this is not an issue. For example, in the case of Kinesis jobs,
+    # we assume that we can have duplicated events in S3. It is not a problem
+    # because the way those events are later imported into Redshift ensures
+    # that they are not imported twice.
+    #
+    # If for some reason we fail to delete the key associated to the lock in
+    # the storage, there is the risk of not releasing the lock ever.
+    # We solve this setting a TTL.
     class DistributedLock
       def initialize(resource, ttl, storage)
         @resource = resource
--- lib/3scale/backend/stats/send_to_kinesis.rb
+++ lib/3scale/backend/stats/send_to_kinesis.rb
@@ -9,30 +9,9 @@ module ThreeScale
         SEND_TO_KINESIS_ENABLED_KEY = 'send_to_kinesis:enabled'.freeze
         private_constant :SEND_TO_KINESIS_ENABLED_KEY
 
-        # We want to avoid having two jobs running at the same time. That could
-        # lead to sending repeated events to Kinesis.
-        # Similarly, we do not want to execute 2 flushes concurrently, or
-        # execute one when a kinesis job is running.
-        #
-        # We use Redis to ensure that using the operation set nx. The locking
-        # algorithm is detailed here: http://redis.io/topics/distlock
-        # Basically, every time that we want to execute a job or a flush,
-        # we generate a random number and set a key (JOB_RUNNING_KEY) with that
-        # random number if its current value is null. If we could set the
-        # value, it means that no other job/flush is running. When the job/flush
-        # finishes, it sets JOB_RUNNING_KEY to null.
-        #
-        # The random number that we use is the current unix epoch in ms. This
-        # does not ensure 100% that two jobs will not be running at the same
-        # time. In our case, this is not a problem. We assume that we can have
-        # duplicated events in S3. Also, 2 jobs could be running at the same
-        # time if there is a problem with the Redis master, but again, this
-        # is not an issue for us.
         JOB_RUNNING_KEY = 'send_to_kinesis:job_running'.freeze
         private_constant :JOB_RUNNING_KEY
 
-        # If for some reason the job fails to set JOB_RUNNING_KEY to 0, other
-        # jobs will not be able to execute. We solve this setting a TTL.
         TTL_JOB_RUNNING_KEY_SEC = 360
         private_constant :TTL_JOB_RUNNING_KEY_SEC
 
