c5f56f2c35a9a2982ae08b667320642aaceed942
David Ortiz <z.david.ortiz@gmail.com>
2016-04-25 14:53:32 +0200

    FailedJobsScheduler: ensure that failed jobs are rescheduled only once
    We need to use a distributed lock. The reason is that the rake task that
    reschedules failed jobs is executed periodically from several worker servers
    and there is the risk that some of them will try to execute it at the same
    time. This could result in rescheduling the same failed job several times.
    
    To understand this, remember that the call to Resque::Failure.requeue does not
    delete a failed job from the queue of failed jobs.

--- lib/3scale/backend/failed_jobs_scheduler.rb
+++ lib/3scale/backend/failed_jobs_scheduler.rb
@@ -1,23 +1,43 @@
 module ThreeScale
   module Backend
     class FailedJobsScheduler
+      TTL_RESCHEDULE_S = 30
+      private_constant :TTL_RESCHEDULE_S
+
       class << self
 
-        # There might be several workers trying to requeue failed jobs at the same
-        # time. This can result in a 'NoMethodError' if one of them calls
-        # Resque::Failure.requeue with an index that is no longer valid.
         def reschedule_failed_jobs
-          begin
-            count = Resque::Failure.count
-            count.times { |i| Resque::Failure.requeue(i) }
-          rescue NoMethodError
-            retry
+          # There might be several callers trying to requeue failed jobs at the
+          # same time. We need to use a lock to avoid rescheduling the same
+          # failed job more than once.
+          key = dist_lock.lock
+
+          rescheduled = 0
+
+          if key
+            begin
+              count = rescheduled = Resque::Failure.count
+              count.times { |i| Resque::Failure.requeue(i) }
+            rescue NoMethodError
+              # The dist lock we use does not guarantee mutual exclusion in all
+              # cases. This can result in a 'NoMethodError' if requeue is
+              # called with an index that is no longer valid.
+              retry
+            end
+
+            Resque::Failure.clear
+            dist_lock.unlock if key == dist_lock.current_lock_key
           end
 
-          Resque::Failure.clear
-          { failed_current: Resque::Failure.count, rescheduled: count }
+          { failed_current: Resque::Failure.count, rescheduled: rescheduled }
         end
 
+        private
+
+        def dist_lock
+          @dist_lock ||= DistributedLock.new(
+              self.name, TTL_RESCHEDULE_S, Storage.instance)
+        end
       end
     end
   end
