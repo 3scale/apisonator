7e5fe1a6210b838a3f9d7748db67c07ec6c8a3c5
David Ortiz <z.david.ortiz@gmail.com>
2016-06-13 15:47:42 +0200

    provider key change use case: use lets and contexts
    To improve readability.

--- spec/use_cases/provider_key_change_use_case_spec.rb
+++ spec/use_cases/provider_key_change_use_case_spec.rb
@@ -4,51 +4,76 @@ module ThreeScale
   module Backend
     describe ProviderKeyChangeUseCase do
       describe '#initialize' do
-        it 'raises an exception on invalid provider keys' do
-          expect { ProviderKeyChangeUseCase.new('foo', 'foo') }
-              .to raise_error(InvalidProviderKeys)
+        errors = { invalid_provider_keys: InvalidProviderKeys,
+                   provider_key_exists: ProviderKeyExists,
+                   provider_key_not_found: ProviderKeyNotFound }
+
+        let(:a_provider_key) { '123' }
+
+        context 'when the provider keys are invalid' do
+          it "raises #{formatted_name(errors[:invalid_provider_keys])}" do
+            expect { ProviderKeyChangeUseCase.new(a_provider_key, a_provider_key) }
+                .to raise_error(errors[:invalid_provider_keys])
+          end
         end
 
-        it 'raises an exception if (new) provider key already exists' do
-          Service.save! id: 7003, provider_key: 'baz'
+        context 'when the new provider key already exists' do
+          let(:existing_key) { 'existing' }
 
-          expect { ProviderKeyChangeUseCase.new('bar', 'baz') }
-              .to raise_error(ProviderKeyExists)
+          before { Service.save! id: 7003, provider_key: existing_key }
+
+          it "raises #{formatted_name(errors[:provider_key_exists])}" do
+            expect { ProviderKeyChangeUseCase.new(a_provider_key, existing_key) }
+                .to raise_error(errors[:provider_key_exists])
+          end
         end
 
-        it 'raises an exception if (old) key doesn\'t exit' do
-          expect { ProviderKeyChangeUseCase.new('baz', 'foo') }
-              .to raise_error(ProviderKeyNotFound)
+        context 'when the old provider key does not exist' do
+          let(:non_existing_key) { 'non_existing' }
+
+          it "raises #{formatted_name(errors[:provider_key_not_found])}" do
+            expect { ProviderKeyChangeUseCase.new(non_existing_key, a_provider_key) }
+                .to raise_error(errors[:provider_key_not_found])
+          end
         end
       end
 
       describe '#process' do
-        let(:use_case) { ProviderKeyChangeUseCase.new('foo', 'bar') }
+        let(:old_key) { 'foo' }
+        let(:new_key) { 'bar' }
+        let(:service_ids_with_old_key) { [7001, 7002] }
+        let(:use_case) { ProviderKeyChangeUseCase.new(old_key, new_key) }
 
         before do
-          Service.save! id: 7001, provider_key: 'foo'
-          Service.save! id: 7002, provider_key: 'foo'
+          service_ids_with_old_key.each do |service_id|
+            Service.save! id: service_id, provider_key: old_key
+          end
           use_case.process
         end
 
         it 'changes the provider key of existing services' do
-          expect(Service.list('bar')).to eq ['7001', '7002']
-          expect(Service.load_by_id(7001).provider_key).to eq 'bar'
-          expect(Service.load_by_id(7002).provider_key).to eq 'bar'
+          expect(Service.list(new_key))
+              .to eq service_ids_with_old_key.map(&:to_s)
+
+          service_ids_with_old_key.each do |service_id|
+            expect(Service.load_by_id(service_id).provider_key).to eq new_key
+          end
         end
 
         it 'sets the default service for the new provider key' do
-          expect(Service.default_id('bar')).to eq '7001'
+          expect(Service.default_id(new_key))
+              .to eq service_ids_with_old_key.first.to_s
         end
 
         it 'removes (old) provider key data' do
-          expect(Service.default_id('foo')).to be nil
-          expect(Service.list('foo')).to be_empty
+          expect(Service.default_id(old_key)).to be nil
+          expect(Service.list(old_key)).to be_empty
         end
 
         it 'changes the provider key associated with the affected services' do
-          expect(Service.provider_key_for(7001)).to eq 'bar'
-          expect(Service.provider_key_for(7002)).to eq 'bar'
+          service_ids_with_old_key.each do |service_id|
+            expect(Service.provider_key_for(service_id)).to eq new_key
+          end
         end
       end
     end
