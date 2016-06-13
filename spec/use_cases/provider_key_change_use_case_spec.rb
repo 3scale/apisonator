3c52ce13b6e592a945fa3fe184d24239c2c90f2b
David Ortiz <z.david.ortiz@gmail.com>
2016-06-13 15:18:20 +0200

    provider key change use case: separate the tests of the 2 methods

--- spec/use_cases/provider_key_change_use_case_spec.rb
+++ spec/use_cases/provider_key_change_use_case_spec.rb
@@ -3,51 +3,54 @@ require_relative '../spec_helper'
 module ThreeScale
   module Backend
     describe ProviderKeyChangeUseCase do
-      let(:use_case){ ProviderKeyChangeUseCase.new('foo', 'bar') }
+      describe '#initialize' do
+        it 'raises an exception on invalid provider keys' do
+          expect { ProviderKeyChangeUseCase.new('foo', 'foo') }
+              .to raise_error(InvalidProviderKeys)
+        end
 
-      before do
-        Service.save! id: 7001, provider_key: 'foo'
-        Service.save! id: 7002, provider_key: 'foo'
-        use_case.process
+        it 'raises an exception if (new) provider key already exists' do
+          Service.save! id: 7003, provider_key: 'baz'
+
+          expect { ProviderKeyChangeUseCase.new('bar', 'baz') }
+              .to raise_error(ProviderKeyExists)
+        end
+
+        it 'raises an exception if (old) key doesn\'t exit' do
+          expect { ProviderKeyChangeUseCase.new('baz', 'foo') }
+              .to raise_error(ProviderKeyNotFound)
+        end
       end
 
-      it 'raises an exception on invalid provider keys' do
-        expect { ProviderKeyChangeUseCase.new('foo', 'foo') }.
-          to raise_error(InvalidProviderKeys)
-      end
+      describe '#process' do
+        let(:use_case) { ProviderKeyChangeUseCase.new('foo', 'bar') }
 
-      it 'raises an exception if (new) provider key already exists' do
-        Service.save! id: 7003, provider_key: 'baz'
+        before do
+          Service.save! id: 7001, provider_key: 'foo'
+          Service.save! id: 7002, provider_key: 'foo'
+          use_case.process
+        end
 
-        expect { ProviderKeyChangeUseCase.new('bar', 'baz') }.
-          to raise_error(ProviderKeyExists)
-      end
+        it 'changes the provider key of existing services' do
+          expect(Service.list('bar')).to eq ['7001', '7002']
+          expect(Service.load_by_id(7001).provider_key).to eq 'bar'
+          expect(Service.load_by_id(7002).provider_key).to eq 'bar'
+        end
 
-      it 'raises an exception if (old) key doesn\'t exit' do
-        expect { ProviderKeyChangeUseCase.new('baz', 'foo') }.
-          to raise_error(ProviderKeyNotFound)
-      end
+        it 'sets the default service for the new provider key' do
+          expect(Service.default_id('bar')).to eq '7001'
+        end
 
-      it 'changes the provider key of existing services' do
-        expect(Service.list('bar')).to eq ['7001', '7002']
-        expect(Service.load_by_id(7001).provider_key).to eq 'bar'
-        expect(Service.load_by_id(7002).provider_key).to eq 'bar'
-      end
+        it 'removes (old) provider key data' do
+          expect(Service.default_id('foo')).to be nil
+          expect(Service.list('foo')).to be_empty
+        end
 
-      it 'sets the default service for the new provider key' do
-        expect(Service.default_id('bar')).to eq '7001'
-      end
-
-      it 'removes (old) provider key data' do
-        expect(Service.default_id('foo')).to be nil
-        expect(Service.list('foo')).to be_empty
-      end
-
-      it 'changes the provider key associated with the affected services' do
-        expect(Service.provider_key_for(7001)).to eq 'bar'
-        expect(Service.provider_key_for(7002)).to eq 'bar'
+        it 'changes the provider key associated with the affected services' do
+          expect(Service.provider_key_for(7001)).to eq 'bar'
+          expect(Service.provider_key_for(7002)).to eq 'bar'
+        end
       end
     end
   end
 end
-
