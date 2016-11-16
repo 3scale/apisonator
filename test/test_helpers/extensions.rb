3e965cc510885beda361e10bf066c43362b65a46
Alejandro Martinez Ruiz <alex@flawedcode.org>
2016-11-16 21:19:25 +0100

    test/helpers/extensions: test '0' and 'false' for no_body
    While at it also drop the bogus requirement for the request to be
    200 OK status.

--- test/test_helpers/extensions.rb
+++ test/test_helpers/extensions.rb
@@ -18,9 +18,20 @@ module TestHelpers
       # 1st: parameters hash to the path endpoint - required
       # 2nd: Rack's environment - optional
       def test_nobody(rack_method, path, &blk)
-        test "call to #{rack_method.upcase} #{path} with no_body option responds 200" do
+        test "call to #{rack_method.upcase} #{path} without no_body responds with a body" do
           # The below 3 lines are repeated in each example. DRYing them up
           # involves some metaprogramming magic. Not worth it just for these.
+          params, env = instance_exec(&blk)
+          params ||= {}
+          env ||= {}
+
+          send(rack_method, path, params, env)
+
+          # _some_ body has to be generated
+          assert_not_equal '', last_response.body
+        end
+
+        test "call to #{rack_method.upcase} #{path} with no_body option responds without body" do
           params, env = instance_exec(&blk)
           params ||= {}
           env ||= {}
@@ -29,30 +40,47 @@ module TestHelpers
             'HTTP_3SCALE_OPTIONS' => ExtensionConstants::NO_BODY
           }))
 
-          assert_equal 200, last_response.status
           assert_equal '', last_response.body
         end
 
-        test "call to #{rack_method.upcase} #{path} with deprecated no_body=1 responds 200" do
+        test "call to #{rack_method.upcase} #{path} with deprecated no_body=1 responds without body" do
           params, env = instance_exec(&blk)
           params ||= {}
           env ||= {}
 
           send(rack_method, path, params.merge({ no_body: 1 }))
 
-          assert_equal 200, last_response.status
           assert_equal '', last_response.body
         end
 
-        test "call to #{rack_method.upcase} #{path} with deprecated no_body=true responds 200" do
+        test "call to #{rack_method.upcase} #{path} with deprecated no_body=true responds without body" do
           params, env = instance_exec(&blk)
           params ||= {}
           env ||= {}
 
           send(rack_method, path, params.merge({ no_body: true }))
 
-          assert_equal 200, last_response.status
           assert_equal '', last_response.body
+        end
+
+        test "call to #{rack_method.upcase} #{path} with deprecated no_body=0 responds with body" do
+          params, env = instance_exec(&blk)
+          params ||= {}
+          env ||= {}
+
+          send(rack_method, path, params.merge({ no_body: 0 }))
+
+          assert_not_equal '', last_response.body
+        end
+
+        test "call to #{rack_method.upcase} #{path} with deprecated no_body=false responds with body" do
+          params, env = instance_exec(&blk)
+          params ||= {}
+          env ||= {}
+
+          send(rack_method, path, params.merge({ no_body: false }))
+
+          assert_not_equal '', last_response.body
         end
       end
     end
