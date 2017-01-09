c27d3adc5d0fad75f0933b399b1d0632a2721e01
David Ortiz <z.david.ortiz@gmail.com>
2017-01-09 12:50:05 +0100

    Move SaaS stats requires to separate file

---
+++ lib/3scale/backend/saas_stats.rb
@@ -0,0 +1,5 @@
+# Requires needed for the SaaS analytics system based on Kinesis + Redshift.
+require '3scale/backend/stats/send_to_kinesis'
+require '3scale/backend/stats/send_to_kinesis_job'
+require '3scale/backend/stats/redshift_importer'
+require '3scale/backend/stats/info'
