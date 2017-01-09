158ecd3c6f2db8d765329358678bf84fcac9257d
David Ortiz <z.david.ortiz@gmail.com>
2017-01-09 12:50:45 +0100

    saas: require SaaS stats classes only if running in SaaS mode

--- lib/3scale/backend/saas.rb
+++ lib/3scale/backend/saas.rb
@@ -3,4 +3,5 @@ if ThreeScale::Backend.configuration.saas
   require '3scale/backend/log_request_storage'
   require '3scale/backend/log_request_cubert_storage'
   require '3scale/backend/transactor/log_request_job'
+  require '3scale/backend/saas_stats'
 end
