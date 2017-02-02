e6ee8631d5becce771c778fab5a6e99f316b9fb8
Alejandro Martinez Ruiz <alex@flawedcode.org>
2017-02-02 11:15:23 +0000

    queue_storage: use Storage helper to employ the new default and config params

--- lib/3scale/backend/queue_storage.rb
+++ lib/3scale/backend/queue_storage.rb
@@ -1,17 +1,16 @@
-require 'redis'
-
 module ThreeScale
   module Backend
     class QueueStorage
       def self.connection(env, cfg)
-        init_params = { driver: :hiredis }
+        init_params = {}
         if !%w(development test).include?(env)
           raise 'Configuration must have a valid queues section' if !valid_cfg?(cfg)
           init_params[:url] = "redis://#{cfg.queues.master_name}"
           sentinels = cfg.queues.sentinels
           init_params[:sentinels] = sentinels unless sentinels.empty?
         end
-        Redis.new(init_params)
+        Redis.new(
+          ThreeScale::Backend::Storage::Helpers.config_with(cfg.queues, init_params))
       end
 
       private
