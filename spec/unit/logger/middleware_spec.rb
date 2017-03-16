e42865fc6b028e0f7079d088d84da2734d2ccb24
David Ortiz <z.david.ortiz@gmail.com>
2017-03-16 11:25:28 +0100

    spec/unit/logger/middleware: define helper to select log writers

--- spec/unit/logger/middleware_spec.rb
+++ spec/unit/logger/middleware_spec.rb
@@ -49,6 +49,40 @@ module ThreeScale
             Timecop.freeze(time) { run_request }
           end
         end
+
+        describe '.writers' do
+          context 'with loggers' do
+            let(:loggers) { described_class.const_get(:WRITERS).keys }
+            let(:writer_classes) { described_class.const_get(:WRITERS).values }
+
+            it 'returns the writers associated with the loggers' do
+              writers = described_class.writers(loggers)
+              expect(writer_classes.all? do |writer_class|
+                writers.find { |writer| writer.is_a?(writer_class) }
+              end).to be true
+            end
+          end
+
+          context 'without loggers' do
+            let(:default_writers) { described_class.const_get(:DEFAULT_WRITERS) }
+
+            it 'returns the default writers' do
+              [nil, []].each do |param|
+                expect(described_class.writers(param)).to eq default_writers
+              end
+            end
+          end
+
+          context 'with an invalid logger' do
+            let(:invalid_logger) { :invalid }
+            invalid_logger_error = described_class::UnsupportedLoggerType
+
+            it "raises #{invalid_logger_error}" do
+              expect { described_class.writers([invalid_logger]) }
+                  .to raise_error(invalid_logger_error)
+            end
+          end
+        end
       end
     end
   end
