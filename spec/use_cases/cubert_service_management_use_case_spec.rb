10328ca2ea0e6993542bb52fcaf4b8b2a40771f1
Alejandro Martinez Ruiz <amr@redhat.com>
2017-03-29 19:46:07 +0100

    spec/cubert_service_management: rework to test that the tracking set is well managed
    This is an awful spec that depended on the order of execution and
    the different ids to hide the fact the implementation contained bugs.

--- spec/use_cases/cubert_service_management_use_case_spec.rb
+++ spec/use_cases/cubert_service_management_use_case_spec.rb
@@ -4,53 +4,72 @@ module ThreeScale
   module Backend
     describe CubertServiceManagementUseCase do
       let(:storage) { ThreeScale::Backend::Storage.instance }
-      let(:enabled_service) { described_class.enable_service '7001', 'foo'; '7001' }
-      let(:disabled_service) { '7002' }
-
-      describe '.disable_service' do
-        before do
-          described_class.disable_service enabled_service
-        end
-
-        it 'deletes the bucket' do
-          expect(described_class.bucket enabled_service).to be nil
-        end
-
-        it 'disables the service' do
-          expect(described_class.enabled? enabled_service).to be false
-        end
-      end
+      let(:service_id) { '7001' }
 
       describe '.enable_service' do
         before do
+          described_class.clean_cubert_redis_keys
           described_class.global_enable
-          described_class.enable_service disabled_service, 'foo'
+          described_class.disable_service service_id
+          described_class.enable_service service_id, 'foo'
         end
 
         it 'enables service' do
-          expect(described_class.enabled? disabled_service).to be_truthy
+          expect(described_class.enabled? service_id).to be_truthy
         end
 
         it 'create a bucket if needed' do
-          expect(described_class.bucket disabled_service).not_to be_nil
+          expect(described_class.bucket service_id).not_to be_nil
         end
 
         it 'assigns a specified bucket' do
-          described_class.enable_service(disabled_service, 'foobar')
-          expect(described_class.bucket disabled_service).to eq('foobar')
+          described_class.enable_service(service_id, 'foobar')
+          expect(described_class.bucket service_id).to eq('foobar')
+        end
+
+        it 'adds an entry to the tracking set' do
+          expect(storage.sismember(
+            described_class.send(:all_bucket_keys_key),
+            described_class.send(:bucket_id_key, service_id))).to be_truthy
+        end
+      end
+
+      describe '.disable_service' do
+        before do
+          described_class.clean_cubert_redis_keys
+          described_class.global_enable
+          described_class.enable_service service_id, 'foo'
+          described_class.disable_service service_id
+        end
+
+        it 'deletes the bucket' do
+          expect(described_class.bucket service_id).to be_nil
+        end
+
+        it 'disables the service' do
+          expect(described_class.enabled? service_id).to be_falsey
+        end
+
+        it 'does not leak an entry in the tracking set' do
+          expect(storage.sismember(
+            described_class.send(:all_bucket_keys_key),
+            described_class.send(:bucket_id_key, service_id))).to be_falsey
         end
       end
 
       describe '.clean_cubert_redis_keys' do
         before do
+          described_class.clean_cubert_redis_keys
           described_class.global_enable
-          enabled_service
+          described_class.enable_service service_id, 'foo'
           described_class.clean_cubert_redis_keys
         end
 
         it 'removes all the keys' do
-          expect(storage.get described_class.send(:global_lock_key)).to be_nil
-          expect(described_class.bucket enabled_service).to be_nil
+          expect(storage.exists described_class.send(:global_lock_key)).to be_falsey
+          expect(storage.exists described_class.send(:global_lock_key)).to be_falsey
+          expect(storage.exists described_class.send(:all_bucket_keys_key)).to be_falsey
+          expect(described_class.bucket service_id).to be_nil
         end
       end
 
