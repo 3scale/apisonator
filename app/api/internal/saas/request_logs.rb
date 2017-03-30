b92f316373ce60b1f86bbf51704c85099ab16d65
Alejandro Martinez Ruiz <amr@redhat.com>
2017-03-30 17:27:26 +0100

    api/internal/request_logs,spec: adapt to changes in how request logs ara managed

--- app/api/internal/saas/request_logs.rb
+++ app/api/internal/saas/request_logs.rb
@@ -1,10 +1,14 @@
 module ThreeScale
   module Backend
     module API
+      # This API should drop the bucket suffix, as it's really a detail
+      # of a deprecated implementation.
       internal_api '/services' do
         put '/:id/logs_bucket' do
-          CubertServiceManagementUseCase.enable_service params[:id], params[:bucket]
-          { status: :ok, bucket: CubertServiceManagementUseCase.bucket(params[:id]) }.to_json
+          # bucket is a deprecated optional param
+          bucket = params[:bucket]
+          CubertServiceManagementUseCase.enable_service params[:id]
+          { status: :ok, bucket: bucket }.to_json
         end
 
         delete '/:id/logs_bucket' do
