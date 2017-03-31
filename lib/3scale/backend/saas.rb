154e073e378356499b012c0fa5a182af0edb522b
Alejandro Martinez Ruiz <amr@redhat.com>
2017-03-31 14:04:51 +0100

    saas: require request_logs to pull in all functionality

--- lib/3scale/backend/saas.rb
+++ lib/3scale/backend/saas.rb
@@ -2,8 +2,7 @@ if ThreeScale::Backend.configuration.saas
   # SaaS-specific dependencies
   require '3scale/backend/statsd'
   require '3scale/backend/experiment'
-  require '3scale/backend/log_request_storage'
-  require '3scale/backend/request_logs/management'
+  require '3scale/backend/request_logs'
   require '3scale/backend/transactor/log_request_job'
   require '3scale/backend/saas_stats'
 end
