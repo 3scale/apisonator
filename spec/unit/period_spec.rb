dfec66dbd79b7fa6afa9d300feb9e917501a0fb3
David Ortiz <z.david.ortiz@gmail.com>
2017-05-29 15:46:56 +0200

    spec/unit/period: add period instance tests

--- spec/unit/period_spec.rb
+++ spec/unit/period_spec.rb
@@ -181,6 +181,20 @@ module ThreeScale
             let(:time) { Time.now.utc }
             subject { klass.new(time) }
 
+            # To compare instances we need to add/subtract to time enough
+            # seconds to jump to the next/previous period.
+            # For seconds +1,-1 works, but not for the rest. For year, which
+            # is the coarsest granularity that we use, we need at least
+            # 60*60*24*366
+            let(:greater) { klass.new(time + 60*60*24*366) }
+            let(:same) { klass.new(time) }
+            let(:less) { klass.new(time - 60*60*24*366) }
+
+            let(:eternity_years) do
+              Period::Boundary::Methods.const_get(:ETERNITY_FINISH).year -
+                  Period::Boundary::Methods.const_get(:ETERNITY_START).year + 1
+            end
+
             include_examples "representable as", "Symbol", :to_sym, granularity.to_sym
             include_examples "representable as", "string", :to_s, granularity.to_s
             include_examples "representable as", "JSON", :as_json, granularity.as_json
@@ -195,7 +209,102 @@ module ThreeScale
               expect(subject.granularity).to be(klass)
             end
 
-            # XXX TODO comparison, succ/pred, start/finish, etc
+            it 'can be compared with other instances that refer to the same granularity' do
+              # Eternity is a special case. Comparison between Eternity
+              # instances always returns true.
+              if granularity == :eternity
+                expect(subject == greater).to be true
+                expect(subject == same).to be true
+                expect(subject == less).to be true
+              else
+                expect(subject < greater).to be true
+                expect(subject == same).to be true
+                expect(subject > less).to be true
+              end
+            end
+
+            it 'defines case equality' do
+              # Second is a special case because === only returns true with
+              # another instance initialized with the same second.
+              if granularity == :second
+                expect(subject === subject.start).to be true
+              else
+                in_range = subject.start + 1
+                out_of_range = subject.start - 1
+                expect(subject === in_range).to be true
+                expect(subject === out_of_range).to be false
+              end
+            end
+
+            it 'has a start' do
+              expect(subject.start).to eq Period::Boundary.start_of(granularity, time)
+            end
+
+            it 'has an end' do
+              expect(subject.finish).to eq Period::Boundary.end_of(granularity, time)
+            end
+
+            it 'returns the remaining time until the end' do
+              granularity_end = Period::Boundary.end_of(granularity, time)
+              expected_remaining = granularity_end - time
+              expect(subject.remaining(time)).to eq expected_remaining
+            end
+
+            it 'has a successor' do
+              expect(subject.succ).to eq klass.new(subject.finish)
+            end
+
+            it 'has a predecessor' do
+              expect(subject.pred).to eq klass.new(subject.start - 1)
+            end
+
+            it 'can get the list of periods contained by the enclosing one' do
+              expected_elements = { second: 60,
+                                    minute: 60,
+                                    hour: 24,
+                                    day: (28..31),
+                                    week: 0,
+                                    month: 12,
+                                    year: eternity_years,
+                                    eternity: 0 }
+
+              res = subject.build_up.entries
+              expected_size = expected_elements[granularity]
+
+              if expected_size.is_a?(Numeric)
+                expect(res.size).to eq expected_size
+              else
+                expect(expected_size).to include res.size
+              end
+
+              expect(res.all? do |period|
+                period.granularity == klass
+              end).to be true
+            end
+
+            it 'can be divided into instances of a more fine grained period' do
+              expected_elements = { second: 0,
+                                    minute: 60,
+                                    hour: 60,
+                                    day: 24,
+                                    week: 7,
+                                    month: (28..31),
+                                    year: 12,
+                                    eternity: eternity_years }
+
+              res = subject.break_down.entries
+              expected_size = expected_elements[granularity]
+
+              if expected_size.is_a?(Numeric)
+                expect(res.size).to eq expected_size
+              else
+                expect(expected_size).to include res.size
+              end
+
+              expect(res.all? do |period|
+                period.granularity == klass.pred
+              end).to be true
+            end
           end
         end
       end
