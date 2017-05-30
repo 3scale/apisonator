a67169f0236aee61b8d14f626e8277fbbda370d5
David Ortiz <z.david.ortiz@gmail.com>
2017-05-30 15:34:16 +0200

    Remove request logs management & storage

--- lib/3scale/backend/saas.rb
+++ lib/3scale/backend/saas.rb
@@ -2,6 +2,5 @@ if ThreeScale::Backend.configuration.saas
   # SaaS-specific dependencies
   require '3scale/backend/statsd'
   require '3scale/backend/experiment'
-  require '3scale/backend/request_logs'
   require '3scale/backend/saas_stats'
 end
