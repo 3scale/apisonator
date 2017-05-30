58043870ecd056c50a9ef8f01b3afa2ab36f9d01
David Ortiz <z.david.ortiz@gmail.com>
2017-05-30 15:22:21 +0200

    Delete request logs job

--- lib/3scale/backend/saas.rb
+++ lib/3scale/backend/saas.rb
@@ -3,6 +3,5 @@ if ThreeScale::Backend.configuration.saas
   require '3scale/backend/statsd'
   require '3scale/backend/experiment'
   require '3scale/backend/request_logs'
-  require '3scale/backend/transactor/log_request_job'
   require '3scale/backend/saas_stats'
 end
