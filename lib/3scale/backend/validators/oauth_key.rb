3d2a8bf93a8f538f4fc411d707dca7ffdf925466
Alejandro Martinez Ruiz <alex@flawedcode.org>
2017-06-01 13:03:35 +0100

    validators/oauth_key: deny present but empty app_key parameter
    This case opens a security problem with some gateway code out there,
    and there is no clear upgrade path for such users. Since newer
    versions never send this parameter as empty, just deny calls with an
    empty parameter.

--- lib/3scale/backend/validators/oauth_key.rb
+++ lib/3scale/backend/validators/oauth_key.rb
@@ -3,7 +3,7 @@ module ThreeScale
     module Validators
       class OauthKey < Base
         def apply
-          if params[:app_key].nil? || params[:app_key].empty? || application.has_key?(params[:app_key])
+          if params[:app_key].nil? || (!params[:app_key].empty? && application.has_key?(params[:app_key]))
             succeed!
           else
             fail!(ApplicationKeyInvalid.new(params[:app_key]))
