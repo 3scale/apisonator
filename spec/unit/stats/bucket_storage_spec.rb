0bcd675f8ec40cb5dce2b169aee6b01d29e3d8d7
David Ortiz <z.david.ortiz@gmail.com>
2017-07-10 11:56:24 +0200

    spec/unit/stats/bucket_storage: bring tests from Info#pending_keys_by_bucket
    need to move the test too.
    Also, the Info module has been deleted, so we can delete its specs.

--- spec/unit/stats/bucket_storage_spec.rb
+++ spec/unit/stats/bucket_storage_spec.rb
@@ -328,6 +328,23 @@ module ThreeScale
             end
           end
         end
+
+        describe '#pending_keys_by_bucket' do
+          context 'without pending buckets' do
+            it { expect(subject.pending_keys_by_bucket).to be_empty }
+            it { expect(subject.pending_keys_by_bucket).to be_kind_of Hash }
+          end
+
+          context 'with pending buckets' do
+            before do
+              storage.zadd(Keys.changed_keys_key, 0, 'foo')
+              storage.sadd(Keys.changed_keys_bucket_key('foo'), '20100101')
+              storage.sadd(Keys.changed_keys_bucket_key('foo'), '20140404')
+            end
+
+            it { expect(subject.pending_keys_by_bucket).to include('foo' => 2) }
+          end
+        end
       end
     end
   end
