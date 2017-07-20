a9a9435604cb94df2df1fb8566e3536f70ef3504
David Ortiz <z.david.ortiz@gmail.com>
2017-07-20 18:11:37 +0200

    Require aws-sdk only when needed
    Stats::Storage is required always, but we only need aws-sdk when
    Analytics is enabled. That is, we need it in SaaS deployments, but not
    for on-premises.

--- lib/3scale/backend/saas_stats.rb
+++ lib/3scale/backend/saas_stats.rb
@@ -1,4 +1,5 @@
 # Requires needed for the SaaS analytics system based on Kinesis + Redshift.
+require 'aws-sdk'
 require '3scale/backend/stats/send_to_kinesis'
 require '3scale/backend/stats/send_to_kinesis_job'
 require '3scale/backend/stats/redshift_importer'
