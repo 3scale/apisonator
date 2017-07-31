b094ece518d0a476f5b0922f6f4cdf48a2985330
David Ortiz <z.david.ortiz@gmail.com>
2017-07-31 17:19:56 +0200

    test/unit/user: test metric names methods
    Adds test for new #load_metric_names and for existing and unstested
    method #metric_name.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 class UserTest < Test::Unit::TestCase
+  include TestHelpers::Sequences
+
   def storage
     @storage ||= Storage.instance(true)
   end
@@ -68,5 +70,53 @@ class UserTest < Test::Unit::TestCase
       assert_equal service.default_user_plan_id, user.plan_id
       assert_equal service.id, user.service_id
     end
+  end
+
+  test '#metric_names returns loaded metric names' do
+    service_id = next_id
+    plan_id = next_id
+    metric_id = next_id
+    metric_name = 'hits'
+    service = Service.save!(provider_key: 'foo', id: service_id,
+                            user_registration_required: false,
+                            default_user_plan_name: 'user_plan_name',
+                            default_user_plan_id: plan_id)
+
+    user = User.load_or_create!(service, 'user_1')
+
+    Metric.save(service_id: service_id, id: metric_id, name: metric_name)
+    UsageLimit.save(service_id: service_id,
+                    plan_id: plan_id,
+                    metric_id: metric_id,
+                    minute: 10)
+
+    # No metrics loaded
+    assert_empty user.metric_names
+
+    user.metric_name(metric_id)
+    assert_equal({ metric_id => metric_name }, user.metric_names)
+  end
+
+  test '#load_metric_names loads and returns the names of all the metrics for '\
+       'which there is a usage limit that applies to the app' do
+    service_id = next_id
+    plan_id = next_id
+    metrics = { next_id => 'metric1', next_id => 'metric2' }
+    service = Service.save!(provider_key: 'foo', id: service_id,
+                            user_registration_required: false,
+                            default_user_plan_name: 'user_plan_name',
+                            default_user_plan_id: plan_id)
+
+    user = User.load_or_create!(service, 'user_1')
+
+    metrics.each do |metric_id, metric_name|
+      Metric.save(service_id: service_id, id: metric_id, name: metric_name)
+      UsageLimit.save(service_id: service_id,
+                      plan_id: plan_id,
+                      metric_id: metric_id,
+                      minute: 10)
+    end
+
+    assert_equal metrics, user.load_metric_names
   end
 end
