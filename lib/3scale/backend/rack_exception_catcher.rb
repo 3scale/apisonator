3e6de7130a00f193170d4e55de435cf963914d58
Eguzki Astiz Lezaun <eastizle@redhat.com>
2017-09-27 18:43:21 +0200

    rack_exception_catcher.rb: customize sinatra response on encoding issues

--- lib/3scale/backend/rack_exception_catcher.rb
+++ lib/3scale/backend/rack_exception_catcher.rb
@@ -16,21 +16,21 @@ module Rack
     }.merge(ThreeScale::Backend::CORS.const_get(:HEADERS)).freeze
     private_constant :ERROR_HEADERS
 
+    INVALID_BYTE_SEQUENCE_ERR_MSG = 'Invalid query parameters: '\
+      'invalid byte sequence in UTF-8'.freeze
+    private_constant :INVALID_BYTE_SEQUENCE_ERR_MSG
+
     def initialize(app, options = {})
       @app = app
       @options = options
     end
 
     def call(env)
-      @app.call(env)
+      resp = @app.call(env)
+      filter_encoding_error_response resp, env
     rescue ThreeScale::Backend::Error => e
       delete_sinatra_error! env
       respond_with e.http_code, prepare_body(e.to_xml, env)
-    rescue TypeError
-      respond_with 400, prepare_body(ThreeScale::Backend::BadRequest.new.to_xml, env)
-    rescue Rack::Utils::InvalidParameterError
-      delete_sinatra_error! env
-      respond_with 400, ThreeScale::Backend::NotValidData.new.to_xml
     rescue Exception => e
       if e.class == RangeError && e.message == "exceeded available parameter key space"
         respond_with 400, ThreeScale::Backend::NotValidData.new.to_xml
@@ -52,7 +52,7 @@ module Rack
       env['sinatra.error'.freeze] = nil
     end
 
-    # Private: Prepares the body to inlude in the reponse.
+    # Private: Prepares the body to include in the reponse.
     #
     # body - Proposed body String.
     # env - The environment Hash.
@@ -62,8 +62,46 @@ module Rack
       ThreeScale::Backend::Listener.threescale_extensions(env)[:no_body] ? ''.freeze : body
     end
 
+    # Returns Rack response.
+    #   Format https://rack.github.io/
+    #   Array with three elements:
+    #   *) The HTTP response code
+    #   *) A Hash of headers
+    #   *) The response body, which must respond to `each`
     def respond_with(code, body)
       [code, ERROR_HEADERS.dup, [body]]
+    end
+
+    # Private:
+    # Filter to transform response under specific conditions:
+    # http_status is 400 and error code refers to encoding issues.
+    # When input request has invalid encoding issues, Sinatra does not raise error
+    # and it prepares its own response. For backwards compatibility, we need
+    # to capture the error and customize response accordingly.
+    # Best and cleanest way to accomplish this is using error handlers. However,
+    # Sinatra 2.0.0 has a bug when registering error handlers and request has encoding issues.
+    # It has been reported in https://github.com/sinatra/sinatra/issues/1350
+    #
+    # resp - Rack response. Format https://rack.github.io/
+    #         Array with three elements:
+    #         *) The HTTP response code
+    #         *) A Hash of headers
+    #         *) The response body, which must respond to `each`
+    #
+    # env - The environment Hash.
+    #
+    # Returns Rack response.
+    def filter_encoding_error_response(resp, env)
+      return resp unless resp.first == 400
+      # According to http://www.rubydoc.info/github/rack/rack/master/file/SPEC#The_Body
+      # The Body must respond to each and must only yield String values.
+      resp_body = resp.last.inject('') { |acc, x| acc << x }
+      if resp_body == INVALID_BYTE_SEQUENCE_ERR_MSG
+        delete_sinatra_error! env
+        # update response
+        resp = respond_with 400, ThreeScale::Backend::NotValidData.new.to_xml
+      end
+      resp
     end
   end
 end
