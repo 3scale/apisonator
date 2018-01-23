565c5f1b555333ab0993476d22e9399a49bbe41e
Alejandro Martinez Ruiz <amr@redhat.com>
2018-01-23 21:10:50 +0000

    queue_storage: use new storage config interface

--- lib/3scale/backend/queue_storage.rb
+++ lib/3scale/backend/queue_storage.rb
@@ -5,12 +5,17 @@ module ThreeScale
         init_params = {}
         if !%w(development test).include?(env)
           raise 'Configuration must have a valid queues section' if !valid_cfg?(cfg)
-          init_params[:url] = "redis://#{cfg.queues.master_name}"
+          init_params[:url] = cfg.queues.master_name
           sentinels = cfg.queues.sentinels
           init_params[:sentinels] = sentinels unless sentinels.empty?
+        else
+          init_params[:default_url] = '127.0.0.1:6379'
         end
-        Redis.new(
-          ThreeScale::Backend::Storage::Helpers.config_with(cfg.queues, init_params))
+
+        options = Backend::Storage::Helpers.config_with(cfg.queues,
+                                                        options: init_params)
+
+        Redis.new options
       end
 
       private
