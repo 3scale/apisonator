ed36183d767b1eda115d91fbfee46c1f499a8b3b
Alejandro Martinez Ruiz <amr@redhat.com>
2018-02-09 17:51:18 +0000

    logging: move Logging module part in logger.rb here

--- lib/3scale/backend/logging.rb
+++ lib/3scale/backend/logging.rb
@@ -4,6 +4,26 @@ require '3scale/backend/logger'
 
 module ThreeScale
   module Backend
+    # include this module to have a handy access to the default logger
+    module Logging
+      def logger
+        @logger ||= ThreeScale::Backend.logger
+      end
+
+      def self.enable!(on:, with: [], as: :logger)
+        logger = if with.empty?
+                   ThreeScale::Backend.logger
+                 else
+                   ThreeScale::Backend::Logger.new(*with).tap do |l|
+                     yield l if block_given?
+                   end
+                 end
+        on.send :define_method, as do
+          logger
+        end
+      end
+    end
+
     class << self
 
       private
