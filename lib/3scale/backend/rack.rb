9a772c7e1b820294081cf8cd7eefefa2bd32e02a
Alejandro Martinez Ruiz <amr@redhat.com>
2018-02-11 19:14:28 +0000

    config.ru: move code to Rack module

---
+++ lib/3scale/backend/rack.rb
@@ -0,0 +1,42 @@
+require '3scale/backend/configuration'
+require '3scale/backend/logging/middleware'
+require '3scale/backend/util'
+require '3scale/backend'
+
+require 'rack'
+
+module ThreeScale
+  module Backend
+    module Rack
+      def self.run(rack)
+        rack.instance_eval do
+          if Backend.configuration.saas && Airbrake.configuration.api_key
+            use Airbrake::Sinatra
+          end
+
+          loggers = Backend.configuration.request_loggers
+          log_writers = Backend::Logging::Middleware.writers loggers
+          use Backend::Logging::Middleware, writers: log_writers
+
+          map "/internal" do
+            require_relative "#{Backend::Util.root_dir}/app/api/api"
+
+            internal_api = Backend::API::Internal.new(
+              username: Backend.configuration.internal_api.user,
+              password: Backend.configuration.internal_api.password,
+              allow_insecure: !Backend.production?
+            )
+
+            use ::Rack::Auth::Basic do |username, password|
+              internal_api.helpers.check_password username, password
+            end if internal_api.helpers.credentials_set?
+
+            run internal_api
+          end
+
+          run Backend::Listener.new
+        end
+      end
+    end
+  end
+end
