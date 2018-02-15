bb0b69fd832dff809f706306b2ae0b4931d557a6
Eguzki Astiz Lezaun <eastizle@redhat.com>
2018-02-15 21:06:28 +0100

    queue_storage: sentinels parsing included in storage helpers module

--- lib/3scale/backend/queue_storage.rb
+++ lib/3scale/backend/queue_storage.rb
@@ -4,10 +4,8 @@ module ThreeScale
       def self.connection(env, cfg)
         init_params = {}
         if !%w(development test).include?(env)
-          raise 'Configuration must have a valid queues section' if !valid_cfg?(cfg)
+          raise 'Configuration must have a valid queues section' unless valid_cfg? cfg
           init_params[:url] = cfg.queues.master_name
-          sentinels = cfg.queues.sentinels
-          init_params[:sentinels] = sentinels unless sentinels.empty?
         else
           init_params[:default_url] = '127.0.0.1:6379'
         end
