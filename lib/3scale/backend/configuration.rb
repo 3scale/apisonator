aff3d575be0bb5f850d509a8829e9fa7d709e6fc
Alejandro Martinez Ruiz <alex@flawedcode.org>
2018-02-19 13:29:06 +0000

    backend: remove no longer needed config.redis.nodes setting

--- lib/3scale/backend/configuration.rb
+++ lib/3scale/backend/configuration.rb
@@ -25,7 +25,7 @@ module ThreeScale
       # Add configuration sections
       config.add_section(:queues, :master_name, :sentinels,
                          :connect_timeout, :read_timeout, :write_timeout)
-      config.add_section(:redis, :url, :proxy, :nodes,
+      config.add_section(:redis, :url, :proxy,
                          :connect_timeout, :read_timeout, :write_timeout)
       config.add_section(:analytics_redis, :server,
                          :connect_timeout, :read_timeout, :write_timeout)
