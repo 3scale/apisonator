c3d7e3e2a9e7794cf50ce876859a15271408de29
David Ortiz <z.david.ortiz@gmail.com>
2019-02-19 10:53:10 +0100

    StorageAsync: add .call_pipeline method to the async-redis lib

---
+++ lib/3scale/backend/storage_async/async_redis.rb
@@ -0,0 +1,19 @@
+# Monkey-patches the async-redis lib to provide a 'call_pipeline' method that
+# sends multiple commands at once and returns an array of the responses for
+# each of them.
+
+module Async
+  module Redis
+    class Client
+      def call_pipeline(commands)
+        @pool.acquire do |connection|
+          commands.each do |command|
+            connection.write_request(command)
+          end
+
+          commands.size.times.map { connection.read_response }
+        end
+      end
+    end
+  end
+end
