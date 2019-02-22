b636e86f75ef09942deff3e9ddc7c8dce7d82b07
David Ortiz <z.david.ortiz@gmail.com>
2019-02-22 12:16:01 +0100

    storage: create StorageSync for the redis-rb based storage
    Also, move the helpers to StorageHelpers to be able to use them to
    instantiate AsyncStorage too.
    
    The helpers will need to be cleaned-up because we have
    ThreeScale::Backend::StorageHelpers and
    ThreeScale::Backend::Storage::Helpers. They should probably be merged.

---
+++ lib/3scale/backend/storage_sync.rb
@@ -0,0 +1,43 @@
+# used to provide a Redis client based on a configuration object
+require 'uri'
+
+module ThreeScale
+  module Backend
+    class StorageSync
+      include Configurable
+
+      class << self
+        # Returns a shared instance of the storage. If there is no instance yet,
+        # creates one first. If you want to always create a fresh instance, set
+        # the +reset+ parameter to true.
+        def instance(reset = false)
+          if reset || @instance.nil?
+            @instance = new(Storage::Helpers.config_with(configuration.redis,
+                            options: get_options))
+          else
+            @instance
+          end
+        end
+
+        private
+
+        def new(options)
+          Redis.new options
+        end
+
+        if ThreeScale::Backend.production?
+          def get_options
+            {}
+          end
+        else
+          DEFAULT_SERVER = '127.0.0.1:22121'.freeze
+          private_constant :DEFAULT_SERVER
+
+          def get_options
+            { default_url: DEFAULT_SERVER }
+          end
+        end
+      end
+    end
+  end
+end
