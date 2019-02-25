66c30da8a147c63d5beef3f94a48435110a51498
David Ortiz <z.david.ortiz@gmail.com>
2019-02-25 16:41:49 +0100

    spec/unit: add specs for Pipeline

---
+++ spec/unit/storage_async/pipeline_spec.rb
@@ -0,0 +1,32 @@
+require '3scale/backend/storage_async'
+
+module ThreeScale
+  module Backend
+    module StorageAsync
+      describe Pipeline do
+        describe '.run' do
+          let(:endpoint) { Async::IO::Endpoint.tcp('localhost', 6379) }
+          let(:async_client) { Async::Redis::Client.new(endpoint) }
+
+          subject { Pipeline.new }
+
+          context 'When the list of commands is empty' do
+            it 'returns an empty array' do
+              expect(subject.run(async_client)).to be_empty
+            end
+          end
+
+          context 'When the list of commands is not empty' do
+            it 'returns an array with the responses in the same order' do
+              subject.call('GET', 'some_key')
+              subject.call('SET', 'some_key', '1')
+              subject.call('GET', 'some_key')
+
+              expect(subject.run(async_client)).to eq([nil, 'OK', '1'])
+            end
+          end
+        end
+      end
+    end
+  end
+end
