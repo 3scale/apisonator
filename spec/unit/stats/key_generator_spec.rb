7fecceb5edbd9dfa85e4c71d843e33b737d9f226
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-02-28 11:39:51 +0100

    stats/key_generator_spec: check unnecessary keys are not generated

--- spec/unit/stats/key_generator_spec.rb
+++ spec/unit/stats/key_generator_spec.rb
@@ -17,106 +17,111 @@ RSpec.describe ThreeScale::Backend::Stats::KeyGenerator do
       to: to.to_i
     }
   end
+  let(:expected_keys_responsecode_service) do
+    %w[200 2XX 403 404 4XX 500 503 5XX].product(%i[hour day week month eternity]).map do |code, gr|
+      ThreeScale::Backend::Stats::Keys.service_response_code_value_key(service_id, code, ThreeScale::Backend::Period[gr].new(from))
+    end
+  end
+  let(:expected_keys_responsecode_application) do
+    codes = %w[200 2XX 403 404 4XX 500 503 5XX]
+    periods = %i[hour day week month year eternity]
+    code_app_period = codes.product(applications, periods)
+    code_app_period.map do |code, app_id, gr|
+      ThreeScale::Backend::Stats::Keys.application_response_code_value_key(service_id, app_id, code, ThreeScale::Backend::Period[gr].new(from))
+    end
+  end
+  let(:expected_keys_responsecode_user) do
+    codes = %w[200 2XX 403 404 4XX 500 503 5XX]
+    periods = %i[hour day week month year eternity]
+    code_app_period = codes.product(applications, periods)
+    code_app_period.map do |code, app_id, gr|
+      ThreeScale::Backend::Stats::Keys.application_response_code_value_key(service_id, app_id, code, ThreeScale::Backend::Period[gr].new(from))
+    end
+  end
+  let(:expected_keys_usage_service) do
+    periods = %i[hour day week month eternity]
+    metric_period = metrics.product(periods)
+    metric_period.map do |metric_id, gr|
+      ThreeScale::Backend::Stats::Keys.service_usage_value_key(service_id, metric_id, ThreeScale::Backend::Period[gr].new(from))
+    end
+  end
+  let(:expected_keys_usage_application) do
+    periods = %i[hour day week month year eternity]
+    metric_app_period = metrics.product(applications, periods)
+    metric_app_period.map do |metric_id, app_id, gr|
+      ThreeScale::Backend::Stats::Keys.application_usage_value_key(service_id, app_id, metric_id, ThreeScale::Backend::Period[gr].new(from))
+    end
+  end
+  let(:expected_keys_usage_user) do
+    periods = %i[hour day week month year eternity]
+    metric_user_period = metrics.product(users, periods)
+    metric_user_period.map do |metric_id, user_id, gr|
+      ThreeScale::Backend::Stats::Keys.user_usage_value_key(service_id, user_id, metric_id, ThreeScale::Backend::Period[gr].new(from))
+    end
+  end
+
   subject { described_class.new(job_params).keys }
 
   context 'responsecode_service keys' do
-    let(:expected_keys) do
-      %w[200 2XX 403 404 4XX 500 503 5XX].product(%i[hour day week month eternity]).map do |code, gr|
-        ThreeScale::Backend::Stats::Keys.service_response_code_value_key(service_id, code, ThreeScale::Backend::Period[gr].new(from))
-      end
-    end
-
     it 'include expected keys' do
-      is_expected.to include(*expected_keys)
+      is_expected.to include(*expected_keys_responsecode_service)
     end
   end
 
   context 'responsecode_application keys' do
-    context 'some applications in the array' do
-      let(:applications) { %w[1] }
-      let(:expected_keys) do
-        codes = %w[200 2XX 403 404 4XX 500 503 5XX]
-        periods = %i[hour day week month year eternity]
-        code_app_period = codes.product(applications, periods)
-        code_app_period.map do |code, app_id, gr|
-          ThreeScale::Backend::Stats::Keys.application_response_code_value_key(service_id, app_id, code, ThreeScale::Backend::Period[gr].new(from))
-        end
-      end
+    let(:applications) { %w[1] }
 
-      it 'include expected keys' do
-        is_expected.to include(*expected_keys)
-      end
+    it 'include expected keys' do
+      is_expected.to include(*expected_keys_responsecode_application)
     end
   end
 
   context 'responsecode_user keys' do
-    context 'some users in the array' do
-      let(:users) { %w[1] }
-      let(:expected_keys) do
-        codes = %w[200 2XX 403 404 4XX 500 503 5XX]
-        periods = %i[hour day week month year eternity]
-        code_app_period = codes.product(applications, periods)
-        code_app_period.map do |code, app_id, gr|
-          ThreeScale::Backend::Stats::Keys.application_response_code_value_key(service_id, app_id, code, ThreeScale::Backend::Period[gr].new(from))
-        end
-      end
+    let(:users) { %w[1] }
 
-      it 'include expected keys' do
-        is_expected.to include(*expected_keys)
-      end
+    it 'include expected keys' do
+      is_expected.to include(*expected_keys_responsecode_user)
     end
   end
 
   context 'usage service keys' do
-    context 'some metrics in the array' do
-      let(:metrics) { %w[1] }
-      let(:expected_keys) do
-        periods = %i[hour day week month eternity]
-        metric_period = metrics.product(periods)
-        metric_period.map do |metric_id, gr|
-          ThreeScale::Backend::Stats::Keys.service_usage_value_key(service_id, metric_id, ThreeScale::Backend::Period[gr].new(from))
-        end
-      end
+    let(:metrics) { %w[1] }
 
-      it 'include expected keys' do
-        is_expected.to include(*expected_keys)
-      end
+    it 'include expected keys' do
+      is_expected.to include(*expected_keys_usage_service)
     end
   end
 
   context 'usage application keys' do
-    context 'some metrics applications in the array' do
-      let(:metrics) { %w[1] }
-      let(:applications) { %w[10] }
-      let(:expected_keys) do
-        periods = %i[hour day week month year eternity]
-        metric_app_period = metrics.product(applications, periods)
-        metric_app_period.map do |metric_id, app_id, gr|
-          ThreeScale::Backend::Stats::Keys.application_usage_value_key(service_id, app_id, metric_id, ThreeScale::Backend::Period[gr].new(from))
-        end
-      end
+    let(:metrics) { %w[1] }
+    let(:applications) { %w[10] }
 
-      it 'include expected keys' do
-        is_expected.to include(*expected_keys)
-      end
+    it 'include expected keys' do
+      is_expected.to include(*expected_keys_usage_application)
     end
   end
 
   context 'usage user keys' do
-    context 'some metrics users in the array' do
-      let(:metrics) { %w[1] }
-      let(:users) { %w[10] }
-      let(:expected_keys) do
-        periods = %i[hour day week month year eternity]
-        metric_user_period = metrics.product(users, periods)
-        metric_user_period.map do |metric_id, user_id, gr|
-          ThreeScale::Backend::Stats::Keys.user_usage_value_key(service_id, user_id, metric_id, ThreeScale::Backend::Period[gr].new(from))
-        end
-      end
+    let(:metrics) { %w[1] }
+    let(:users) { %w[10] }
 
-      it 'include expected keys' do
-        is_expected.to include(*expected_keys)
-      end
+    it 'include expected keys' do
+      is_expected.to include(*expected_keys_usage_user)
+    end
+  end
+  context 'usage user keys' do
+    let(:metrics) { %w[1] }
+    let(:users) { %w[10] }
+    let(:applications) { %w[100] }
+    it 'unexpected keys not found' do
+      expect(subject.count).to eq([
+        expected_keys_responsecode_service.count,
+        expected_keys_responsecode_application.count,
+        expected_keys_responsecode_user.count,
+        expected_keys_usage_service.count,
+        expected_keys_usage_application.count,
+        expected_keys_usage_user.count
+      ].reduce(:+))
     end
   end
 end
