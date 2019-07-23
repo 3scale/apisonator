43b90a124c75b3627487c52b40afb292319ed478
David Ortiz <z.david.ortiz@gmail.com>
2019-07-23 16:43:15 +0200

    config/schedule: use ruby 2.4

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-job_type :backend_rake, 'cd :path && /usr/local/bin/bundle exec rake2.2 :task --silent'
+job_type :backend_rake, 'cd :path && /usr/local/bin/bundle exec rake2.4 :task --silent'
 
 every 2.minutes do
   backend_rake 'stats:kinesis:send', path: '/home/bender/backend'
