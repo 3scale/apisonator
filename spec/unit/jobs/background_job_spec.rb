701b0e3b465c80c6ffb96fc197a48f8386af6a44
David Ortiz <z.david.ortiz@gmail.com>
2019-07-30 18:25:16 +0200

    background_job: update Prometheus metrics

--- spec/unit/jobs/background_job_spec.rb
+++ spec/unit/jobs/background_job_spec.rb
@@ -1,8 +1,11 @@
 require_relative '../../spec_helper'
+require '3scale/backend/worker_metrics'
 
 module ThreeScale
   module Backend
     describe BackgroundJob do
+      include SpecHelpers::ConfigHelper
+
       class FooJob < BackgroundJob
 
         def self.perform_logged(*args)
@@ -42,6 +45,34 @@ module ThreeScale
         it 'complains when you don\'t set a log message' do
           expect { BarJob.perform() }.to raise_error(
             BackgroundJob::Error, 'No job message given')
+        end
+      end
+
+      describe '.perform' do
+        after(:all) { reset_worker_prometheus_metrics_state }
+
+        context 'when Prometheus metrics are enabled' do
+          before { enable_worker_prometheus_metrics }
+
+          it 'reports type of job performed and runtime' do
+            expect(WorkerMetrics).to receive(:increase_job_count).with('FooJob')
+
+            expect(WorkerMetrics).to receive(:report_runtime)
+                                 .with('FooJob', satisfy { |v| v > 0 })
+
+            FooJob.perform(Time.now.getutc.to_f)
+          end
+        end
+
+        context 'when Prometheus metrics are disabled' do
+          before { disable_worker_prometheus_metrics }
+
+          it 'does not update any Prometheus metric' do
+            expect(WorkerMetrics).not_to receive(:increase_job_count)
+            expect(WorkerMetrics).not_to receive(:report_runtime)
+
+            FooJob.perform(Time.now.getutc.to_f)
+          end
         end
       end
     end
