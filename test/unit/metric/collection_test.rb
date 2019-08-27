b2457bd43113c943f285a92069578cdcf3cb1b38
David Ortiz <z.david.ortiz@gmail.com>
2019-08-27 11:04:04 +0200

    test/unit/metric/collection: test hierarchies with n_levels >2

--- test/unit/metric/collection_test.rb
+++ test/unit/metric/collection_test.rb
@@ -2,6 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
 
 module Metric
   class CollectionTest < Test::Unit::TestCase
+    include TestHelpers::Sequences
+    include TestHelpers::MetricsHierarchy
+
     def setup
       Storage.instance(true).flushdb
       Memoizer.reset!
@@ -86,6 +89,29 @@ module Metric
       assert_equal({'2001' => '#6', '2002' => '#6'}, metrics.process_usage('hits_child_1' => '#6'))
       assert_equal({'2001' => '#11', '2002' => '#6', '2003' => '#11'}, metrics.process_usage('hits_child_1' => '#6', 'hits_child_2' => '#11'))
             
+    end
+
+    def test_process_usage_handles_hierarchies_with_more_than_2_levels
+      # This test generates a hierarchy with more than 2 levels and one metric
+      # per level.
+      # Also, it creates a usage where the metrics at the last and last + 1
+      # levels of the hierarchy have 1 hit. Then, it checks that those hits are
+      # correctly propagated, that is, all the metrics in the hierarchy should
+      # have 2 hits, except the one at the bottom which should have only 1.
+
+      service_id = next_id
+      levels = rand(3..10)
+      metrics = gen_hierarchy_one_metric_per_level(service_id, levels)
+
+      metrics_collection = Metric::Collection.new(service_id)
+      processed = metrics_collection.process_usage(
+        metrics.last.name => 1, metrics[-2].name => 1
+      )
+
+      assert_equal(1, processed[metrics.last.id])
+      assert_true metrics[0..-2].all? do |metric|
+        processed[metric.id] == 2
+      end
     end
   end
 end
