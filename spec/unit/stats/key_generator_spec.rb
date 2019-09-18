440fdf25fe0d4bc1bd1fce01853a27d1eb3a2405
David Ortiz <z.david.ortiz@gmail.com>
2019-09-18 22:28:11 +0200

    spec/unit: remove support for end-users

--- spec/unit/stats/key_generator_spec.rb
+++ spec/unit/stats/key_generator_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe ThreeScale::Backend::Stats::KeyGenerator do
   let(:service_id) { '123456' }
   let(:applications) { %w[] }
   let(:metrics) { %w[] }
-  let(:users) { %w[] }
   let(:from) { Time.new(2002, 10, 31) }
   let(:to) { Time.new(2002, 11, 30) }
   let(:job_params) do
@@ -16,7 +15,6 @@ RSpec.describe ThreeScale::Backend::Stats::KeyGenerator do
       service_id: service_id,
       applications: applications,
       metrics: metrics,
-      users: users,
       from: from.to_i,
       to: to.to_i
     }
@@ -29,16 +27,6 @@ RSpec.describe ThreeScale::Backend::Stats::KeyGenerator do
     end
   end
   let(:expected_keys_responsecode_application) do
-    codes = %w[200 2XX 403 404 4XX 500 503 5XX]
-    periods = %i[hour day week month year eternity]
-    code_app_period = codes.product(applications, periods)
-    code_app_period.flat_map do |code, app_id, gr|
-      periods(gr, from, to).flat_map do |period|
-        ThreeScale::Backend::Stats::Keys.application_response_code_value_key(service_id, app_id, code, period)
-      end
-    end
-  end
-  let(:expected_keys_responsecode_user) do
     codes = %w[200 2XX 403 404 4XX 500 503 5XX]
     periods = %i[hour day week month year eternity]
     code_app_period = codes.product(applications, periods)
@@ -66,16 +54,6 @@ RSpec.describe ThreeScale::Backend::Stats::KeyGenerator do
       end
     end
   end
-  let(:expected_keys_usage_user) do
-    periods = %i[hour day week month year eternity]
-    metric_user_period = metrics.product(users, periods)
-    metric_user_period.flat_map do |metric_id, user_id, gr|
-      periods(gr, from, to).flat_map do |period|
-        ThreeScale::Backend::Stats::Keys.user_usage_value_key(service_id, user_id, metric_id, period)
-      end
-    end
-  end
-
   subject { described_class.new(job_params).keys }
 
   context 'responsecode_service keys' do
@@ -89,14 +67,6 @@ RSpec.describe ThreeScale::Backend::Stats::KeyGenerator do
 
     it 'include expected keys' do
       is_expected.to include(*expected_keys_responsecode_application)
-    end
-  end
-
-  context 'responsecode_user keys' do
-    let(:users) { %w[1] }
-
-    it 'include expected keys' do
-      is_expected.to include(*expected_keys_responsecode_user)
     end
   end
 
@@ -114,30 +84,6 @@ RSpec.describe ThreeScale::Backend::Stats::KeyGenerator do
 
     it 'include expected keys' do
       is_expected.to include(*expected_keys_usage_application)
-    end
-  end
-
-  context 'usage user keys' do
-    let(:metrics) { %w[1] }
-    let(:users) { %w[10] }
-
-    it 'include expected keys' do
-      is_expected.to include(*expected_keys_usage_user)
-    end
-  end
-  context 'usage user keys' do
-    let(:metrics) { %w[1] }
-    let(:users) { %w[10] }
-    let(:applications) { %w[100] }
-    it 'unexpected keys not found' do
-      expect(subject.count).to eq([
-        expected_keys_responsecode_service.count,
-        expected_keys_responsecode_application.count,
-        expected_keys_responsecode_user.count,
-        expected_keys_usage_service.count,
-        expected_keys_usage_application.count,
-        expected_keys_usage_user.count
-      ].reduce(:+))
     end
   end
 end
--- spec/unit/stats/keys_spec.rb
+++ spec/unit/stats/keys_spec.rb
@@ -10,15 +10,12 @@ module ThreeScale
         private_class_method :currify
 
         Application = Struct.new(:service_id, :id)
-        User = Struct.new(:service_id, :username)
         app = Application.new 1000, 10
-        user = User.new 1000, 20
 
         let(:app)            { app }
         let(:service_id)     { app.service_id }
         let(:application_id) { app.id }
         let(:metric_id)      { 100 }
-        let(:user_id)        { user.username }
         let(:time)           { Time.utc(2014, 7, 29, 18, 25) }
 
         describe '.service_key_prefix' do
@@ -36,15 +33,6 @@ module ThreeScale
 
           it 'returns a composed key with application id' do
             expect(result).to eq("stats/{service:1000}/cinstance:10")
-          end
-        end
-
-        describe '.user_key_prefix' do
-          let(:prefix) { "stats/{service:1000}"}
-          let(:result) { Keys.user_key_prefix(prefix, user_id) }
-
-          it 'returns a composed key with user id' do
-            expect(result).to eq("stats/{service:1000}/uinstance:20")
           end
         end
 
@@ -92,10 +80,6 @@ module ThreeScale
 
         describe '.application_usage_value_key' do
           it_behaves_like 'usage keys', currify(:application_usage_value_key, app.service_id, app.id), "cinstance:#{app.id}"
-        end
-
-        describe '.user_usage_value_key' do
-          it_behaves_like 'usage keys', currify(:user_usage_value_key, app.service_id, user.username), "uinstance:#{user.username}"
         end
 
         describe '.counter_key' do
