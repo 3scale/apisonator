70a61558defa8239d438fd22167db69e05e056f1
David Ortiz <z.david.ortiz@gmail.com>
2019-10-09 14:33:21 +0200

    storage_async/async_redis: remove unnecessary monkey-patch
    The patch is now in the upstream.

--- lib/3scale/backend/storage_async/async_redis.rb
+++ lib/3scale/backend/storage_async/async_redis.rb
@@ -8,40 +8,12 @@ module Async
       def call_pipeline(commands)
         @pool.acquire do |connection|
           commands.each do |command|
-            connection.write_request_without_flush(command)
+            connection.write_request(command)
           end
 
           connection.flush
 
           commands.size.times.map { connection.read_response }
-        end
-      end
-    end
-  end
-end
-
-module Async
-  module Redis
-    module Protocol
-      class RESP
-
-        # This is exactly the same as #write_request but without flushing at
-        # the end. It leaves the responsibility of flushing to the caller.
-        # This method is useful for pipelining because instead of flushing
-        # once per command, we want to flush just once for the whole pipeline
-        # for performance reasons.
-        def write_request_without_flush(arguments)
-          write_lines("*#{arguments.count}")
-
-          arguments.each do |argument|
-            string = argument.to_s
-
-            write_lines("$#{string.bytesize}", string)
-          end
-        end
-
-        def flush
-          @stream.flush
         end
       end
     end
