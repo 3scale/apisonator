9c819ae2b3c24e3abafe2e1853cda08b9b993bb0
David Ortiz <z.david.ortiz@gmail.com>
2020-03-24 13:41:15 +0100

    spec/server/listener_metrics: test with falcon

--- spec/server/listener_metrics_spec.rb
+++ spec/server/listener_metrics_spec.rb
@@ -6,8 +6,8 @@ module ThreeScale
   module Backend
     describe Listener do
       # The port is not the standard one to avoid clashes with other tests
-      let(:puma_port) { 4000 }
-      let(:puma_host) { 'localhost' }
+      let(:listener_port) { 4000 }
+      let(:listener_host) { 'localhost' }
       let(:config_file) { '/tmp/.3scale_backend_listener_metrics_test.config' }
       let(:metrics_endpoint) { '/metrics' }
       let(:metrics_port) { 9394 }
@@ -32,73 +32,93 @@ module ThreeScale
           }
         end
 
-        before do
-          write_config_file(config_file)
-          start_puma_with_metrics(config_file, puma_port, metrics_port)
+        shared_examples_for 'listener with metrics' do |server|
+          before do
+            write_config_file(config_file)
+            start_listener(config_file, true, listener_port, metrics_port, server)
 
-          ThreeScale::Backend::Service.save!(
-            provider_key: provider_key, id: service_id
-          )
-          ThreeScale::Backend::Application.save(
-            service_id: service_id, id: app_id, state: :active
-          )
-          ThreeScale::Backend::Application.save_id_by_key(
-            service_id, user_key, app_id
-          )
-          ThreeScale::Backend::Metric.save(
-            service_id: service_id, id: metric_id, name: metric_name
-          )
+            ThreeScale::Backend::Service.save!(
+              provider_key: provider_key, id: service_id
+            )
+            ThreeScale::Backend::Application.save(
+              service_id: service_id, id: app_id, state: :active
+            )
+            ThreeScale::Backend::Application.save_id_by_key(
+              service_id, user_key, app_id
+            )
+            ThreeScale::Backend::Metric.save(
+              service_id: service_id, id: metric_id, name: metric_name
+            )
 
-          # Do requests to generate some metrics:
-          # - 1 authorize (authorized)
-          # - 2 authreps authorized
-          # - 2 authreps unauthorized, one that returns 403 and another that returns 404
-          do_auth(puma_host, puma_port, args)
-          2.times { do_authrep(puma_host, puma_port, args) }
-          do_authrep(puma_host, puma_port, args.merge(user_key: 'invalid')) # 403
-          do_authrep(puma_host, puma_port, args.merge(metric_name: 'invalid')) # 404
+            # Do requests to generate some metrics:
+            # - 1 authorize (authorized)
+            # - 2 authreps authorized
+            # - 2 authreps unauthorized, one that returns 403 and another that returns 404
+            do_auth(listener_host, listener_port, args)
+            2.times { do_authrep(listener_host, listener_port, args) }
+            do_authrep(listener_host, listener_port, args.merge(user_key: 'invalid')) # 403
+            do_authrep(listener_host, listener_port, args.merge(metric_name: 'invalid')) # 404
+          end
+
+          after do
+            stop_listener(listener_port, server)
+            delete_config_file(config_file)
+          end
+
+          it 'shows metrics in Prometheus format' do
+            metrics_resp = Net::HTTP.get(listener_host, metrics_endpoint, metrics_port)
+
+            # These are some lines that we know that should be part of the output.
+            check_lines = [
+              '# TYPE apisonator_listener_response_codes counter',
+              '# HELP apisonator_listener_response_codes Response codes',
+              'apisonator_listener_response_codes{request_type="authrep",resp_code="2xx"} 2.0',
+              'apisonator_listener_response_codes{request_type="authorize",resp_code="2xx"} 1.0',
+              'apisonator_listener_response_codes{request_type="authrep",resp_code="403"} 1.0',
+              'apisonator_listener_response_codes{request_type="authrep",resp_code="404"} 1.0',
+              '# TYPE apisonator_listener_response_times_seconds histogram',
+              '# HELP apisonator_listener_response_times_seconds Response times',
+              'apisonator_listener_response_times_seconds_count{request_type="authorize"} 1.0',
+              'apisonator_listener_response_times_seconds_count{request_type="authrep"} 4.0',
+            ]
+
+            expect(metrics_resp).to include(*check_lines)
+          end
         end
 
-        after do
-          stop_puma(puma_port)
-          delete_config_file(config_file)
+        context 'running Puma' do
+          it_behaves_like 'listener with metrics', :puma
         end
 
-        it 'shows metrics in Prometheus format' do
-          metrics_resp = Net::HTTP.get(puma_host, metrics_endpoint, metrics_port)
-
-          # These are some lines that we know that should be part of the output.
-          check_lines = [
-            '# TYPE apisonator_listener_response_codes counter',
-            '# HELP apisonator_listener_response_codes Response codes',
-            'apisonator_listener_response_codes{request_type="authrep",resp_code="2xx"} 2.0',
-            'apisonator_listener_response_codes{request_type="authorize",resp_code="2xx"} 1.0',
-            'apisonator_listener_response_codes{request_type="authrep",resp_code="403"} 1.0',
-            'apisonator_listener_response_codes{request_type="authrep",resp_code="404"} 1.0',
-            '# TYPE apisonator_listener_response_times_seconds histogram',
-            '# HELP apisonator_listener_response_times_seconds Response times',
-            'apisonator_listener_response_times_seconds_count{request_type="authorize"} 1.0',
-            'apisonator_listener_response_times_seconds_count{request_type="authrep"} 4.0',
-          ]
-
-          expect(metrics_resp).to include(*check_lines)
+        context 'running Falcon' do
+          it_behaves_like 'listener with metrics', :falcon
         end
       end
 
       context 'when listener metrics are not enabled' do
-        before do
-          write_config_file(config_file)
-          start_puma_without_metrics(config_file, puma_port)
+        shared_examples_for 'listener without metrics' do |server|
+          before do
+            write_config_file(config_file)
+            start_listener(config_file, false, listener_port, metrics_port, server)
+          end
+
+          after do
+            stop_listener(listener_port, server)
+            delete_config_file(config_file)
+          end
+
+          it 'does not open the metrics port' do
+            expect { Net::HTTP.get(listener_host, metrics_endpoint, metrics_port) }
+              .to raise_error(Errno::EADDRNOTAVAIL)
+          end
         end
 
-        after do
-          stop_puma(puma_port)
-          delete_config_file(config_file)
+        context 'running Puma' do
+          it_behaves_like 'listener without metrics', :puma
         end
 
-        it 'does not open the metrics port' do
-          expect { Net::HTTP.get(puma_host, metrics_endpoint, metrics_port) }
-                 .to raise_error(Errno::EADDRNOTAVAIL)
+        context 'running Falcon' do
+          it_behaves_like 'listener without metrics', :falcon
         end
       end
 
@@ -117,15 +137,7 @@ module ThreeScale
         File.delete(path)
       end
 
-      def start_puma_with_metrics(config_file, puma_port, metrics_port)
-        start_puma(config_file, true, puma_port, metrics_port)
-      end
-
-      def start_puma_without_metrics(config_file, puma_port)
-        start_puma(config_file, false, puma_port, '')
-      end
-
-      def start_puma(config_file, metrics_enabled, puma_port, metrics_port)
+      def start_listener(config_file, metrics_enabled, listener_port, metrics_port, server)
         envs = {
           LISTENER_WORKERS: 2, # To check that metrics are accurate with multiple workers
           CONFIG_LISTENER_PROMETHEUS_METRICS_ENABLED: metrics_enabled,
@@ -135,23 +147,31 @@ module ThreeScale
         envs_str = envs.map { |env, val| "#{env}=#{val}" }.join(' ')
 
         # Send logs to /dev/null to avoid cluttering the output
-        start_ok = system("#{envs_str} bundle exec bin/3scale_backend start -p #{puma_port} > /dev/null &")
+        start_ok = system("#{envs_str} bundle exec bin/3scale_backend " +
+                          "-s #{server} start -p #{listener_port} 2> /dev/null &")
         raise 'Failed to start Puma' unless start_ok
         sleep(2) # Give it some time to be ready
       end
 
-      def stop_puma(puma_port)
-        system("bundle exec bin/3scale_backend stop -p #{puma_port}")
+      def stop_listener(port, server)
+        if server == :puma
+          system("bundle exec bin/3scale_backend stop -p #{port}")
+        else # stop not implemented in Falcon
+          # Need to send 2 SIGTERMs because a bug in falcon v0.35.x
+          # https://github.com/socketry/falcon/issues/109
+          2.times { system("pkill -u #{Process.euid} -f \"ruby .*falcon\"") }
+        end
+        sleep(2) # Give it some time to stop
       end
 
-      def do_auth(puma_host, puma_port, args)
+      def do_auth(listener_host, listener_port, args)
         query = auth_query(args)
-        Net::HTTP.get(puma_host, query, puma_port)
+        Net::HTTP.get(listener_host, query, listener_port)
       end
 
-      def do_authrep(puma_host, puma_port, args)
+      def do_authrep(listener_host, listener_port, args)
         query = authrep_query(args)
-        Net::HTTP.get(puma_host, query, puma_port)
+        Net::HTTP.get(listener_host, query, listener_port)
       end
 
       def auth_query(args)
