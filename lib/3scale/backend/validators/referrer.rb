df2301736ca72e4275cf9d94ee4c2944405b03b4
David Ortiz <z.david.ortiz@gmail.com>
2020-04-09 17:29:27 +0200

    validators/referrer: allow to configure legacy behavior

--- lib/3scale/backend/validators/referrer.rb
+++ lib/3scale/backend/validators/referrer.rb
@@ -2,6 +2,37 @@ module ThreeScale
   module Backend
     module Validators
       class Referrer < Base
+
+        # There are some users that depend on a previous version of the
+        # pattern_matches? method. We make this configurable to avoid breaking
+        # changes for those users.
+
+        class << self
+          def define_pattern_match(legacy)
+            class_eval do
+              if legacy
+                def pattern_matches?(pattern, value)
+                  /#{pattern}/ =~ value
+                end
+              else
+                def pattern_matches?(pattern, value)
+                  /\A#{pattern}\z/ =~ value
+                end
+              end
+            end
+          end
+          private :define_pattern_match
+
+          def behave_as_legacy(legacy)
+            # don't blow up if we still didn't define it
+            remove_method :pattern_matches? rescue NameError
+            define_pattern_match legacy
+          end
+          private :behave_as_legacy unless ThreeScale::Backend.test? # leave it public for tests
+        end
+
+        behave_as_legacy ThreeScale::Backend.configuration.legacy_referrer_filters
+
         BYPASS = '*'.freeze
         private_constant :BYPASS
 
@@ -20,14 +51,8 @@ module ThreeScale
         private
 
         def matches?(pattern, value)
-          if value == BYPASS
-            true
-          else
-            pattern = Regexp.escape(pattern)
-            pattern = pattern.gsub('\*', '.*')
-
-            /\A#{pattern}\z/ =~ value
-          end
+          return true if value == BYPASS
+          pattern_matches?(Regexp.escape(pattern).gsub('\*', '.*'), value)
         end
       end
     end
