4bd15ca9d3b4e7dd4132441c9312b3663ec719ab
David Ortiz <z.david.ortiz@gmail.com>
2020-09-07 18:27:47 +0200

    test/unit/transactor/notify_job: stop sending old timestamps
    Notify jobs with an old timestamp raise `TransactionTimestampTooOld`.
    The current tests are not failing because they either mock the call that
    ends up raising that error or because they test other errors that are
    checked before the timestamp.
    Let's change this to stop using old timestamps. It will be less
    confusing.

--- test/unit/transactor/notify_job_test.rb
+++ test/unit/transactor/notify_job_test.rb
@@ -11,62 +11,71 @@ module Transactor
     end
 
     def test_processes_the_transactions
+      now = Time.now.utc
+
       Transactor::ProcessJob.expects(:perform).
         with([{:service_id     => @master_service_id,
                :application_id => @provider_application_id,
-               :timestamp      => Time.utc(2010, 7, 29, 18, 21),
+               :timestamp      => now,
                :usage          => {@master_hits_id => 1, @master_authorizes_id => 1}}])
 
       Transactor::NotifyJob.perform(@provider_key,
                                     {'transactions/authorize' => 1},
-                                    Time.utc(2010, 7, 29, 18, 21),
-                                    Time.utc(2010, 7, 29, 18, 21).to_f)
+                                    now,
+                                    now.to_f)
     end
 
     def test_does_not_raise_an_exception_if_provider_key_is_invalid
+      now = Time.now.utc
+
       assert_nothing_raised do
         Transactor::NotifyJob.perform('foo',
                                       {'transactions/authorize' => 1},
-                                      Time.utc(2010, 7, 29, 18, 21),
-                                      Time.utc(2010, 7, 29, 18, 21).to_f)
+                                      now,
+                                      now.to_f)
       end
     end
 
     def test_does_not_report_error_if_provider_key_is_invalid
+      now = Time.now.utc
       ErrorStorage.expects(:store).never
 
       Transactor::NotifyJob.perform('foo',
                                     {'transactions/authorize' => 1},
-                                    Time.utc(2010, 7, 29, 18, 21),
-                                    Time.utc(2010, 7, 29, 18, 21).to_f)
+                                    now,
+                                    now.to_f)
     end
 
     def test_does_not_process_the_transactions_if_provider_key_is_invalid
+      now = Time.now.utc
       Transactor::ProcessJob.expects(:perform).never
 
       Transactor::NotifyJob.perform('foo',
                                     {'transactions/authorize' => 1},
-                                    Time.utc(2010, 7, 29, 18, 21),
-                                    Time.utc(2010, 7, 29, 18, 21).to_f)
+                                    now,
+                                    now.to_f)
     end
 
     def test_raises_an_exception_if_metrics_are_invalid
+      now = Time.now.utc
+
       assert_raises MetricInvalid do
         Transactor::NotifyJob.perform(@provider_key,
                                       {'transactions/invalid_metric' => 1},
-                                      Time.utc(2010, 7, 29, 18, 21),
-                                      Time.utc(2010, 7, 29, 18, 21).to_f)
+                                      now,
+                                      now.to_f)
       end
     end
 
     def test_does_not_process_the_transactions_if_metrics_are_invalid
+      now = Time.now.utc
       Transactor::ProcessJob.expects(:perform).never
 
       begin
         Transactor::NotifyJob.perform(@provider_key,
                                       {'transactions/invalid_metric' => 1},
-                                      Time.utc(2010, 7, 29, 18, 21),
-                                      Time.utc(2010, 7, 29, 18, 21).to_f)
+                                      now,
+                                      now.to_f)
       rescue MetricInvalid
         # ...
       end
