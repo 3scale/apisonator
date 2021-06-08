c596be319557285e3a0cd8d6d2f6b14e94828231
Alejandro Martinez Ruiz <alex@flawedcode.org>
2021-06-08 03:13:59 +0200

    test/helpers/sequences: ensure next_id always produces UTF8
    This is not strictly needed, but when an expectation fails the clutter
    about this being US-ASCII instead of UTF-8 can be avoided.

--- test/test_helpers/sequences.rb
+++ test/test_helpers/sequences.rb
@@ -5,7 +5,7 @@ module TestHelpers
     # Generates unique id each time it's called.
     def next_id
       @@last_id += 1
-      @@last_id.to_s
+      @@last_id.to_s.encode(Encoding::UTF_8)
     end
   end
 end
