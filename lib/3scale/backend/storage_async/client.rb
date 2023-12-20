e928e848bf2681e5b6c7e7e8ff7da9cda9347cd6
Joan Lledó <jlledo@redhat.com>
2023-12-20 13:23:28 +0100

    Fix TLS connection in async mode

--- lib/3scale/backend/storage_async/client.rb
+++ lib/3scale/backend/storage_async/client.rb
@@ -117,11 +117,23 @@ module ThreeScale
             tcp_endpoint
           when 'rediss'
             ssl_context = OpenSSL::SSL::SSLContext.new
-            ssl_context.set_params(opts[:ssl_params])
+            ssl_context.set_params(format_ssl_params(opts[:ssl_params]))
             Async::IO::SSLEndpoint.new(tcp_endpoint, ssl_context: ssl_context)
           else
             raise ArgumentError
           end
+        end
+
+        def format_ssl_params(ssl_params)
+          cert = ssl_params[:cert].to_s.strip
+          key = ssl_params[:key].to_s.strip
+          return ssl_params if cert.empty? && key.empty?
+
+          updated_ssl_params = ssl_params.dup
+          updated_ssl_params[:cert] = OpenSSL::X509::Certificate.new(File.read(cert))
+          updated_ssl_params[:key] = OpenSSL::PKey.read(File.read(key))
+
+          updated_ssl_params
         end
       end
     end
