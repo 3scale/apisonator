1f199efa64a0722fe530c5dfe7fd897ac1cb1c63
Joan Lledó <jlledo@redhat.com>
2024-02-02 10:18:57 +0100

    Fix job_queues_spec suite
    It was sharing fibers across threads

--- spec/spec_helpers/worker_helper.rb
+++ spec/spec_helpers/worker_helper.rb
@@ -4,8 +4,8 @@ module SpecHelpers
       # The async worker creates a reactor and all the tests are run within a
       # reactor (see the config.around(:each) call on spec_helper.rb). To avoid
       # the worker creating a nested reactor inside the one already running, we
-      # run Worker.work in its own thread.
-      Thread.new { ThreeScale::Backend::Worker.work(one_off: true) }.join
+      # run Worker.work under a new reactor
+      Sync { ThreeScale::Backend::Worker.work(one_off: true) }
     end
   end
 end
