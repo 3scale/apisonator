d914f434dc96e64716882c1e42081f05833b16bb
Joan Lledó <jlledo@redhat.com>
2024-02-08 12:19:11 +0100

    Add tests for #clear_queue

--- spec/unit/worker_async_spec.rb
+++ spec/unit/worker_async_spec.rb
@@ -1,3 +1,4 @@
+require_relative '../spec_helper'
 require '3scale/backend/worker_async'
 
 module ThreeScale
@@ -41,6 +42,54 @@ module ThreeScale
           it 'sends fail() to the job' do
             subject.work
             expect(test_job).to have_received(:fail).with(error)
+          end
+        end
+
+        describe 'when there are pending jobs in the memory queue' do
+          subject do
+            WorkerAsync.new
+          end
+
+          let(:test_job) { instance_double('BackgroundJob') }
+          let(:n_reports) { 10 }
+          let(:queue) { Queue.new.tap { |q| n_reports.times.map { q.push test_job } } }
+
+          before do
+            subject.instance_variable_set(:@jobs, queue)
+          end
+
+          it 'clears the queue'do
+            expect(subject).to receive(:perform).exactly(n_reports).times.with(test_job)
+            queue.close
+
+            subject.send(:clear_queue)
+          end
+
+          it 'clears the queue also when new jobs are added during the execution' do
+            n_new_reports = 5
+            expect(subject).to receive(:perform).exactly(n_reports + n_new_reports).times.with(test_job)
+
+            # The thread will process all jobs in the queue and wait forever until the queue is closed
+            thread = Thread.new { Sync { subject.send(:clear_queue) } }
+            Sync do
+              barrier = Async::Barrier.new
+              n_new_reports.times { barrier.async { queue.push test_job } }
+              barrier.wait # We don't want to close the queue while there are still tasks pushing jobs to it
+            end
+            queue.close # Unlock the thread
+
+            # Interrupt the test if the thread is locked for more than 10 seconds
+            # We assume something went wrong
+            t_start = Time.now
+            while thread.alive?
+              if Time.now - t_start > 10
+                raise 'The worker is taking too much to process the jobs'
+              end
+
+              sleep(0.1)
+            end
+
+            thread.join
           end
         end
       end
