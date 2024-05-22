dea998b7a7611de992c12aff09531429487507a2
Joan Lledó <jlledom@member.fsf.org>
2024-05-22 14:22:04 +0200

    THREESCALE-11061: Async mode: Add support for Redis logical DBs (#390)
    * Add support for Redis logical DBs in async mode

---
+++ lib/3scale/backend/async_redis/protocol/extended_resp2.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'async/redis/protocol/resp2'
+
+module ThreeScale
+  module Backend
+    module AsyncRedis
+      module Protocol
+
+        # Custom Redis Protocol supporting Redis logical DBs
+        class ExtendedRESP2
+          def initialize(db: nil)
+            @db = db
+          end
+
+          def client(stream)
+            client = Async::Redis::Protocol::RESP2.client(stream)
+
+            if @db
+              client.write_request(["SELECT", @db])
+              client.read_response
+            end
+
+            client
+          end
+        end
+      end
+    end
+  end
+end
