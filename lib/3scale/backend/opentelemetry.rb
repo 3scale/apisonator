4fb6a255f8c651e93b0a0b06c7135ab7d4b7f6df
Joan Lledó <jlledom@member.fsf.org>
2024-09-13 10:24:56 +0200

    Better service name
    Co-authored-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- lib/3scale/backend/opentelemetry.rb
+++ lib/3scale/backend/opentelemetry.rb
@@ -3,6 +3,6 @@ require 'opentelemetry/instrumentation/sinatra'
 require 'opentelemetry-exporter-otlp'
 
 OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'backend'
+  c.service_name = '3scale-backend'
   c.use 'OpenTelemetry::Instrumentation::Sinatra'
 end if ThreeScale::Backend.configuration.opentelemetry.enabled
