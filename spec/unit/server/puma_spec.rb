bf476250390efd6a4607211c9a428a15cd130c7f
Aleksandar N. Kostadinov <akostadinov@gmail.com>
2025-10-15 19:44:56 +0300

    puma example with real-world parameters we use

--- spec/unit/server/puma_spec.rb
+++ spec/unit/server/puma_spec.rb
@@ -3,7 +3,7 @@ require '3scale/backend/server/puma'
 
 describe ThreeScale::Backend::Server::Puma do
   describe '.start' do
-    let(:global_options) { { manifest: { server_model: { workers: 2, min_threads: 1, max_threads: 1 } } } }
+    let(:global_options) { { manifest: { server_model: { workers: 1, min_threads: 1, max_threads: 1 } } } }
 
     it 'includes bind parameter when bind is specified' do
       options = { bind: '127.0.0.1', port: '3000' }
